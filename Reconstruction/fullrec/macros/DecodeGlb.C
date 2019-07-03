a8ab2296cc10004c1302dfc4b1e8881a23d2f280
Christian Finck <cfinck@iphc.cnrs.fr>
2019-07-03 13:52:44 +0200

    Add macro for global reconstruction

---
+++ Reconstruction/fullrec/macros/DecodeGlb.C
@@ -0,0 +1,43 @@
+// Macro to make local reconstruction
+// Ch. Finck
+
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+
+#include <TString.h>
+#include <TStopwatch.h>
+
+#include "GlobalPar.hxx"
+#include "GlobalReco.h"
+
+#endif
+
+
+void DecodeGlb(TString name = "data/data_built.2211.physics_foot.daq.VTX.1.dat")
+{
+   GlobalPar::Instance();
+   GlobalPar::GetPar()->Print();
+   
+   Int_t pos = name.Last('.');
+   TString nameOut = name(0, pos);
+   nameOut.Append("_Out.root");
+   
+   GlobalReco* glbRec = new GlobalReco(name, nameOut);
+   
+   // global setting
+   glbRec->EnableTree();
+   glbRec->EnableHisto();
+   glbRec->EnableTracking();
+
+   
+   TStopwatch watch;
+   watch.Start();
+   
+   glbRec->BeforeEventLoop();
+   glbRec->LoopEvent(500);
+   glbRec->AfterEventLoop();
+
+   watch.Print();
+}
+
+
