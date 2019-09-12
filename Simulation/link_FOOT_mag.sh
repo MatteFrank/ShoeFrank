8f6a6c8010fd9e0f235f4a3595e21e9ec8d5af8b
Serena Marta Valle <serena.valle@mi.infn.it>
2019-09-12 18:01:27 +0200

    Corrected ldpm3qmd in ldpmqmd for fluka pro users

--- Simulation/link_FOOT_mag.sh
+++ Simulation/link_FOOT_mag.sh
@@ -9,7 +9,7 @@ $FLUPRO/flutil/fff  magfld.f
 $FLUPRO/flutil/fff  mgdraw_lib.f
 $FLUPRO/flutil/fff  UpdateCurrentParticle.f
 
-$FLUPRO/flutil/ldpm3qmd -m fluka usrini.o usrout.o usreou.o usrein.o  mgdraw.o magfld.o mgdraw_lib.o UpdateCurrentParticle.o -o fluka_FOOT_mag.exe
+$FLUPRO/flutil/ldpmqmd -m fluka usrini.o usrout.o usreou.o usrein.o  mgdraw.o magfld.o mgdraw_lib.o UpdateCurrentParticle.o -o fluka_FOOT_mag.exe
 
 rm -rf  *.o 
 
