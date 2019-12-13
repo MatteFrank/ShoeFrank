8b0b8769bcfede6b6ca0b2c3e325c3d3933799bb
Christian Finck <cfinck@iphc.cnrs.fr>
2019-12-13 09:42:46 +0100

    Re-intend

--- libs/src/TAVTbase/TAVTactBaseTrack.hxx
+++ libs/src/TAVTbase/TAVTactBaseTrack.hxx
@@ -71,46 +71,46 @@ protected:
    Bool_t AppyCuts(TAVTbaseTrack* track) ;
    
 protected:
-   TAGdataDsc*     fpNtuTrack;		     // input data dsc
-   TAGdataDsc*     fpNtuClus;		     // output data dsc
-   TAGparaDsc*     fpConfig;		     // configuration dsc
-   TAGparaDsc*     fpGeoMap;		     // geometry para dsc
-   TAGparaDsc*     fpCalib;		         // calib para dsc
-   TAGgeoTrafo*    fpFootGeo;	         // First geometry transformer
+   TAGdataDsc*     fpNtuTrack;		    // input data dsc
+   TAGdataDsc*     fpNtuClus;		       // output data dsc
+   TAGparaDsc*     fpConfig;		       // configuration dsc
+   TAGparaDsc*     fpGeoMap;		       // geometry para dsc
+   TAGparaDsc*     fpCalib;		       // calib para dsc
+   TAGgeoTrafo*    fpFootGeo;	          // First geometry transformer
 	
-   Int_t           fTracksMaximum;       // maximum number of tracks allowed
-   Int_t           fRequiredClusters;    //! number of clusters required to make a track
-   Double_t        fSearchClusDistance;  //! Max distance to associate a track and a cluster
-   Float_t         fPlaneResolution;     // plane resolution
+   Int_t           fTracksMaximum;      // maximum number of tracks allowed
+   Int_t           fRequiredClusters;   //! number of clusters required to make a track
+   Double_t        fSearchClusDistance; //! Max distance to associate a track and a cluster
+   Float_t         fPlaneResolution;    // plane resolution
       
-   TGraphErrors*    fGraphU;             // pointer to graph for fit in U    
-   TGraphErrors*    fGraphV;             // pointer to graph for fit in V  
+   TGraphErrors*   fGraphU;             // pointer to graph for fit in U
+   TGraphErrors*   fGraphV;             // pointer to graph for fit in V
       
-   TH1F*            fpHisPixelTot;		 // Total number of pixels per tracked cluster
-   TH1F*            fpHisPixel[36];		 // Total number of pixels per tracked cluster for each sensor
-   TH2F*            fpHisTrackMap[36];    // track map per sensor
-   TH1F*            fpHisResX[36];        // Residual in X
-   TH1F*            fpHisResY[36];        // Residual in Y
-   TH1F*            fpHisResTotX;        // Total Residual in X
-   TH1F*            fpHisResTotY;        // Total Residual in Y
-   TH1F*            fpHisChi2TotX;       // Total Chi2 in X direction
-   TH1F*            fpHisChi2TotY;       // Total Chi2 in Y direction
-   TH1F*            fpHisTrackEvt;       // number of track per event
-   TH1F*            fpHisTrackClus;      // number of clusters per track
-   TH1F*            fpHisClusSensor;     // number of tracked clusters per sensor
-   TH1F*            fpHisClusLeft;       // number of clusters left (not tracked)
-   TH2F*            fpHisClusLeftPix;    // number of clusters left (not tracked) vs # pixels
-   TH1F*            fpHisTheta;          // Polar angular distribution
-   TH1F*            fpHisPhi;            // Azimutal angular distribution
-   TH1F*            fpHisMeanPixel;      // Mean number of pixels per tracked cluster
-   TH1F*            fpHisMeanCharge;     // Mwean Charge per tracked cluster
-   TH2F*            fpHisBeamProf;       // VTX Beam profile extrapolated to target
+   TH1F*           fpHisPixelTot;		 // Total number of pixels per tracked cluster
+   TH1F*           fpHisPixel[36];		 // Total number of pixels per tracked cluster for each sensor
+   TH2F*           fpHisTrackMap[36];   // track map per sensor
+   TH1F*           fpHisResX[36];       // Residual in X
+   TH1F*           fpHisResY[36];       // Residual in Y
+   TH1F*           fpHisResTotX;        // Total Residual in X
+   TH1F*           fpHisResTotY;        // Total Residual in Y
+   TH1F*           fpHisChi2TotX;       // Total Chi2 in X direction
+   TH1F*           fpHisChi2TotY;       // Total Chi2 in Y direction
+   TH1F*           fpHisTrackEvt;       // number of track per event
+   TH1F*           fpHisTrackClus;      // number of clusters per track
+   TH1F*           fpHisClusSensor;     // number of tracked clusters per sensor
+   TH1F*           fpHisClusLeft;       // number of clusters left (not tracked)
+   TH2F*           fpHisClusLeftPix;    // number of clusters left (not tracked) vs # pixels
+   TH1F*           fpHisTheta;          // Polar angular distribution
+   TH1F*           fpHisPhi;            // Azimutal angular distribution
+   TH1F*           fpHisMeanPixel;      // Mean number of pixels per tracked cluster
+   TH1F*           fpHisMeanCharge;     // Mwean Charge per tracked cluster
+   TH2F*           fpHisBeamProf;       // VTX Beam profile extrapolated to target
    
-   TString           fPrefix;            // prefix of histogram
-   TString           fTitleDev;          // device name for histogram title
+   TString         fPrefix;             // prefix of histogram
+   TString         fTitleDev;           // device name for histogram title
 
 protected:
-   static Bool_t    fgRefit;             // flag to update parameter during tracking   
+   static Bool_t    fgRefit;            // flag to update parameter during tracking
       
    ClassDef(TAVTactBaseTrack,1)
 };
