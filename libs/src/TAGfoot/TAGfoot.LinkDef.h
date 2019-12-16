8b2b17a10e45063cd70029b8ff208fbc884750a2
Christian Finck <cfinck@iphc.cnrs.fr>
2019-12-16 17:23:11 +0100

    Put local reconstruction as member not hesitance

--- libs/src/TAGfoot/TAGfoot.LinkDef.h
+++ libs/src/TAGfoot/TAGfoot.LinkDef.h
@@ -26,8 +26,6 @@
 #pragma link C++ class  TAGntuGlbTrack;
 #pragma link C++ class  TAGactNtuGlbTrack;
 #pragma link C++ class  GlobalReco;
-#pragma link C++ class  GlobalRecoMC;
 #pragma link C++ class  GlobalToeReco;
-#pragma link C++ class  GlobalToeRecoMC;
 
 #endif
