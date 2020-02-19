735920d2090e04851c4dc1bffe79fae5a78837fb
Christian Finck <cfinck@iphc.cnrs.fr>
2020-02-19 12:04:09 +0100

    Correct parameter for VTX action when BM track present

--- libs/src/TAGfoot/BaseReco.cxx
+++ libs/src/TAGfoot/BaseReco.cxx
@@ -435,15 +435,14 @@ void BaseReco::CreateRecActionVtx()
          fActTrackVtx->CreateHistogram();
       
       if (GlobalPar::GetPar()->IncludeTG()) {
-	  if(GlobalPar::GetPar()->IncludeBM()) {
-	    fActVtx    = new TAVTactNtuVertexPD("vtActVtx", fpNtuTrackVtx, fpNtuVtx, fpParConfVtx, fpParGeoVtx, fpParGeoG);
-	    if (fFlagHisto)
-	      fActVtx->CreateHistogram();
-	  } else {
-	    fActVtx    = new TAVTactNtuVertexPD("vtActVtx", fpNtuTrackVtx, fpNtuVtx, fpParConfVtx, fpParGeoVtx, fpParGeoG, fpNtuTrackBm);
-	    if (fFlagHisto)
-	      fActVtx->CreateHistogram();
-	  }
+         if(GlobalPar::GetPar()->IncludeBM()) {
+            fActVtx = new TAVTactNtuVertexPD("vtActVtx", fpNtuTrackVtx, fpNtuVtx, fpParConfVtx, fpParGeoVtx, fpParGeoG, fpNtuTrackBm);
+            
+         } else
+            fActVtx = new TAVTactNtuVertexPD("vtActVtx", fpNtuTrackVtx, fpNtuVtx, fpParConfVtx, fpParGeoVtx, fpParGeoG);
+         
+         if (fFlagHisto)
+            fActVtx->CreateHistogram();
       }
    }
 }
