8d38f0eec5b9b4c6dde02b17639205a4d29fd335
Alexandre Sécher <asecher@iphc.cnrs.fr>
2020-02-25 10:08:34 +0100

    added checker to help with the amount of data generated to debug reconstruction

---
+++ libs/src/TATOEbase/TATOEchecker.cxx
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+//                                                        //
+// Class Description of TATOEchecker                      //
+//                                                        //
+////////////////////////////////////////////////////////////
+
+//
+//File      : TATOEchecker.cpp
+//Author    : Alexandre Sécher (alexandre.secher@iphc.cnrs.fr)
+//Date      : 10/02/2020
+//Framework : PhD thesis, CNRS/IPHC/DRHIM/Hadrontherapy, Strasbourg, France
+//
+
+
+#include "TATOEchecker.hxx"
+
+
+
+
