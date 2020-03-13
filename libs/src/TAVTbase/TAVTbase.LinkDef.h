e60a99229faca1c67d8f772e8ef472f44dfe5c6c
Christian Finck <cfinck@iphc.cnrs.fr>
2020-03-13 17:57:03 +0100

    Revert by hand

--- libs/src/TAVTbase/TAVTbase.LinkDef.h
+++ libs/src/TAVTbase/TAVTbase.LinkDef.h
@@ -4,6 +4,11 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ class TAVTntuRaw+;
+
+#pragma link C++ class TAVTbaseNtuHit+;
+#pragma link C++ class TAVTntuHit+;
+
 #pragma link C++ class TAVTbaseParMap+;
 #pragma link C++ class TAVTparMap+;
 
@@ -16,20 +21,38 @@
 #pragma link C++ class TAVTparCal+;
 #pragma link C++ class TAVTbaseParCal+;
 
-#pragma link C++ class TAVTntuRaw+;
-#pragma link C++ class TAVTbaseNtuHit+;
-#pragma link C++ class TAVTntuHit+;
+
+#pragma link C++ class TAVTactBaseRaw+;
+#pragma link C++ class TAVTactBaseNtuRaw+;
+#pragma link C++ class TAVTactVmeReader+;
+#pragma link C++ class TAVTactVmeWriter+;
+#pragma link C++ class TAVTactStdRaw+;
+#pragma link C++ class TAVTactStdDaqRaw+;
+#pragma link C++ class TAVTactNtuRaw+;
+
+#pragma link C++ class TAVTactBaseNtuMC+;
+#pragma link C++ class TAVTactNtuMC+;
 
 #pragma link C++ class TAVTntuCluster+;
 #pragma link C++ class TAVTcluster+;
 #pragma link C++ class TAVTbaseCluster+;
 
+#pragma link C++ class TAVTactBaseNtuCluster+;
+#pragma link C++ class TAVTactNtuClusterF+;
+
 #pragma link C++ class TAVTntuTrack+;
 #pragma link C++ class TAVTbaseTrack+;
 #pragma link C++ class TAVTtrack+;
+#pragma link C++ class TAVTactBaseTrack+;
+#pragma link C++ class TAVTactBaseNtuTrack+;
+#pragma link C++ class TAVTactNtuTrack+;
+#pragma link C++ class TAVTactNtuTrackF+;
 
 #pragma link C++ class TAVTntuVertex+;
 #pragma link C++ class TAVTvertex+;
+#pragma link C++ class TAVTactBaseNtuVertex+;
+#pragma link C++ class TAVTactNtuVertex+;
+#pragma link C++ class TAVTactNtuVertexPD+;
 
 
 #endif
