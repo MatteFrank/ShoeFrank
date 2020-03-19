504b831b2c8e401693a353ead8ec6aca54747f43
Christian Finck <cfinck@iphc.cnrs.fr>
2020-03-19 17:20:00 +0100

    Put getter in const Disentangle class member

--- libs/src/TABMbase/TABMparMap.cxx
+++ libs/src/TABMbase/TABMparMap.cxx
@@ -38,7 +38,7 @@ TABMparMap::TABMparMap() {
   //~ chaPlane = tchaPlane;
   //~ chaView =  tchaView; 
   //~ chaSense = tchaSense;
-  tdc_maxcha=64;
+  fTdcMaxCha=64;
   
 }
 
@@ -74,20 +74,20 @@ Bool_t TABMparMap::FromFile(const TString& name, TABMparGeo *bmgeo) {
       //      Info("FromFile()","Skip comment line:: %s",bufConf);
     }else if(strchr(bufConf,'#')) {
       sscanf(bufConf, "#%d %d %d %d %d",&myArg1,&myArg2,&myArg3,&myArg4, &myArg5);
-       if(myArg1>-1 && myArg1<tdc_maxcha && myArg2<36 && myArg2>-1 && (myArg3>=0 || myArg3<=5) && (myArg4==1 || myArg4==0) && myArg5>-1 && myArg5<3) {   
+       if(myArg1>-1 && myArg1<fTdcMaxCha && myArg2<36 && myArg2>-1 && (myArg3>=0 || myArg3<=5) && (myArg4==1 || myArg4==0) && myArg5>-1 && myArg5<3) {
         //~ chaBoID.push_back(myArg2);
         //~ chaPlane.push_back(myArg3);
         //~ chaView.push_back(myArg4);
         //~ chaSense.push_back(myArg5);
         //~ tdc2cell_map.insert(std::make_pair(myArg1,bmgeo->GetBMNcell(myArg2,myArg3,myArg4)));
-        if(tdc2cell_vec[myArg1]<0){
+        if(fTdc2CellVec[myArg1]<0){
           if(bmgeo->GetBMNcell(myArg3,myArg4,myArg5)!=myArg2)
             Error("TABMparMap::FromFile()","channel wrong channel id and identifiers!!!!");
           else{
-            //~ tdc2cell_vec[myArg1]=bmgeo->GetBMNcell(myArg2,myArg3,myArg4);
-            //~ cell2tdc_vec[bmgeo->GetBMNcell(myArg2,myArg3,myArg4)]=myArg1;
-            tdc2cell_vec[myArg1]=myArg2;
-            cell2tdc_vec[myArg2]=myArg1;
+            //~ fTdc2CellVec[myArg1]=bmgeo->GetBMNcell(myArg2,myArg3,myArg4);
+            //~ fCell2TdcVec[bmgeo->GetBMNcell(myArg2,myArg3,myArg4)]=myArg1;
+            fTdc2CellVec[myArg1]=myArg2;
+            fCell2TdcVec[myArg2]=myArg1;
           }
         }
         else{
@@ -96,55 +96,55 @@ Bool_t TABMparMap::FromFile(const TString& name, TABMparGeo *bmgeo) {
         }
       } else {
         Error(""," TABMparMap Plane Map Error:: check config file!! (#)");
-        cout<<"myArg1="<<myArg1<<"  myArg2="<<myArg2<<"  myArg3="<<myArg3<<"  myArg4="<<myArg4<<"  myArg5="<<myArg5<<"  tdc_maxcha="<<tdc_maxcha<<endl;
+        cout<<"myArg1="<<myArg1<<"  myArg2="<<myArg2<<"  myArg3="<<myArg3<<"  myArg4="<<myArg4<<"  myArg5="<<myArg5<<"  fTdcMaxCha="<<fTdcMaxCha<<endl;
         return kTRUE;
       }
     } else if(strchr(bufConf,'T')) {
         sscanf(bufConf, "T %d",&myArg1);
-      if(myArg1<0 || myArg1>tdc_maxcha) {
+      if(myArg1<0 || myArg1>fTdcMaxCha) {
         Error("FromFile()","TABMparMap::Reference Tr channel:: check config file!!(T)");
         return kTRUE;
       }else{
-        trefCh=myArg1;
-        tdc2cell_vec[trefCh]=-1000;
-        cell2tdc_vec[36]=trefCh;
+        fTrefCh=myArg1;
+        fTdc2CellVec[fTrefCh]=-1000;
+        fCell2TdcVec[36]=fTrefCh;
       }
     } else if(strchr(bufConf,'S')) {
-        sscanf(bufConf, "S %d",&sca830ch);
+        sscanf(bufConf, "S %d",&fSca830ch);
     } else if(strchr(bufConf,'A')) {
-        sscanf(bufConf, "A %d",&adc792ch);
+        sscanf(bufConf, "A %d",&fAdc792ch);
     } else if(strchr(bufConf,'H')) {
         sscanf(bufConf, "H %d",&myArg1);
       if(myArg1<0) {
-        Error("FromFile()","TABMparMap::hm_ev_read ERROR:: check config file!!(H)");
+        Error("FromFile()","TABMparMap::fhmEvRead ERROR:: check config file!!(H)");
         return kTRUE;
       }else
-        hm_ev_read=myArg1;
+        fhmEvRead=myArg1;
     } else if(strchr(bufConf,'M')) {
         sscanf(bufConf, "M %d",&myArg1);
         if(!(myArg1==64 || myArg1==128 || myArg1==256)) {
-          Error("FromFile()","TABMparMap::tdc_maxcha not = 64 || 128 || 256:: check config file!!(M)");
+          Error("FromFile()","TABMparMap::fTdcMaxCha not = 64 || 128 || 256:: check config file!!(M)");
           return kTRUE;
         }else{
-          tdc_maxcha=myArg1;
-          for(Int_t i=0;i<tdc_maxcha;i++)
-            tdc2cell_vec.push_back(-1);
+          fTdcMaxCha=myArg1;
+          for(Int_t i=0;i<fTdcMaxCha;i++)
+            fTdc2CellVec.push_back(-1);
           for(Int_t i=0;i<37;i++)
-            cell2tdc_vec.push_back(-1);
+            fCell2TdcVec.push_back(-1);
           }
     }
   }
 
-  board_num=513; //512 is fixed + 1=tdc (mandatory)
-  if(sca830ch>=0)
-    board_num+= 256;
-  if(adc792ch>=0)
-    board_num+= 16;
+  fboardNum=513; //512 is fixed + 1=tdc (mandatory)
+  if(fSca830ch>=0)
+    fboardNum+= 256;
+  if(fAdc792ch>=0)
+    fboardNum+= 16;
     
   //check on the map
   Int_t tmp_int(0);
-  for(Int_t i=0;i<tdc_maxcha;i++)
-    if(tdc2cell_vec[i]>=0)
+  for(Int_t i=0;i<fTdcMaxCha;i++)
+    if(fTdc2CellVec[i]>=0)
       tmp_int++;
   if(tmp_int!=36){
     cout<<"ERROR in TABMparMap:FromFile():error in the map! you haven't set 36 channel for the BM! number of channel set="<<tmp_int<<endl;
@@ -152,12 +152,12 @@ Bool_t TABMparMap::FromFile(const TString& name, TABMparGeo *bmgeo) {
   }
   
   if(FootDebugLevel(1)){
-    cout<<"TABMparMap::print tdc2cell_vec"<<endl;
-    for(Int_t i=0;i<tdc2cell_vec.size();i++)
-      cout<<"i="<<i<<"  tdc2cell_vec[i]="<<tdc2cell_vec[i]<<endl;
-    cout<<endl<<"print cell2tdc_vec"<<endl;
-    for(Int_t i=0;i<cell2tdc_vec.size();i++)
-      cout<<"i="<<i<<"   cell2tdc_vec[i]="<<cell2tdc_vec[i]<<endl;
+    cout<<"TABMparMap::print fTdc2CellVec"<<endl;
+    for(Int_t i=0;i<fTdc2CellVec.size();i++)
+      cout<<"i="<<i<<"  fTdc2CellVec[i]="<<fTdc2CellVec[i]<<endl;
+    cout<<endl<<"print fCell2TdcVec"<<endl;
+    for(Int_t i=0;i<fCell2TdcVec.size();i++)
+      cout<<"i="<<i<<"   fCell2TdcVec[i]="<<fCell2TdcVec[i]<<endl;
     cout<<endl;
   }
   
@@ -174,8 +174,8 @@ void TABMparMap::Clear(Option_t*){
   //~ chaPlane.clear();
   //~ chaView.clear(); 
   //~ chaSense.clear();
-  tdc2cell_vec.clear();
-  cell2tdc_vec.clear();
+  fTdc2CellVec.clear();
+  fCell2TdcVec.clear();
   return;
 }
 
