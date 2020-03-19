e2561492a9a6fedd7b6bf21d88d6efd0c5e33c9f
Christian Finck <cfinck@iphc.cnrs.fr>
2020-03-19 09:55:57 +0100

    Put Getter in const Disentangle data members

--- libs/src/TABMbase/TABMntuTrackTr.cxx
+++ libs/src/TABMbase/TABMntuTrackTr.cxx
@@ -11,10 +11,10 @@ ClassImp(TABMntuTrackTr);
 //! Default constructor.
 
 TABMntuTrackTr::TABMntuTrackTr():
-    nhit(0), chi2Red(999), isConverged(0),eff_fittedplane(-1)
+    fHitsN(0), fChi2Red(999), fIsConverged(0),fEffFittedPlane(-1)
 {
-  Pvers.SetXYZ(-100.,-100.,-100.);
-  R0.SetXYZ(-100.,-100.,-100.);
+  fPvers.SetXYZ(-100.,-100.,-100.);
+  fR0.SetXYZ(-100.,-100.,-100.);
  }
 
 //------------------------------------------+-----------------------------------
@@ -29,9 +29,9 @@ TABMntuTrackTr::~TABMntuTrackTr(){}
 void TABMntuTrackTr::Dump() const
 { 
   cout<<endl<<"------------ Dump Track Class ---------"<<endl;
-  cout<<"new tracking: nhit="<<nhit<<"  chi2Red="<<chi2Red<<"  isConverged="<<isConverged<<"  eff_fittedplane="<<eff_fittedplane<<endl;
-  cout<<"Pvers=("<<Pvers.X()<<", "<<Pvers.Y()<<", "<<Pvers.Z()<<")"<<endl;
-  cout<<"R0=("<<R0.X()<<", "<<R0.Y()<<", "<<R0.Z()<<")"<<endl;
+  cout<<"new tracking: nhits="<<fHitsN<<"  chi2Red="<<fChi2Red<<"  isConverged="<<fIsConverged<<"  eff_fittedplane="<<fEffFittedPlane<<endl;
+  cout<<"Pvers=("<<fPvers.X()<<", "<<fPvers.Y()<<", "<<fPvers.Z()<<")"<<endl;
+  cout<<"R0=("<<fR0.X()<<", "<<fR0.Y()<<", "<<fR0.Z()<<")"<<endl;
 }
 
 /*-----------------------------------------------------------------*/
@@ -40,27 +40,41 @@ void TABMntuTrackTr::Dump() const
 void TABMntuTrackTr::Clean()
 {
   //new tracking
-  nhit=0;
-  chi2Red=999;
-  isConverged=0;
-  eff_fittedplane=-1;
-  R0.SetXYZ(-100.,-100.,-100.);
-  Pvers.SetXYZ(-100.,-100.,-100.);
+  fHitsN=0;
+  fChi2Red=999;
+  fIsConverged=0;
+  fEffFittedPlane=-1;
+  fR0.SetXYZ(-100.,-100.,-100.);
+  fPvers.SetXYZ(-100.,-100.,-100.);
   
 }
 
-TABMntuTrackTr::TABMntuTrackTr(const TABMntuTrackTr &tr_in){
-  nhit=tr_in.nhit;
-  chi2Red=tr_in.chi2Red;
-  isConverged=tr_in.isConverged;
-  eff_fittedplane=tr_in.eff_fittedplane;
-  R0=tr_in.R0;
-  Pvers=tr_in.Pvers;
+TABMntuTrackTr::TABMntuTrackTr(const TABMntuTrackTr &tr_in)
+{
+  fHitsN=tr_in.fHitsN;
+  fChi2Red=tr_in.fChi2Red;
+  fIsConverged=tr_in.fIsConverged;
+  fEffFittedPlane=tr_in.fEffFittedPlane;
+  fR0=tr_in.fR0;
+  fPvers=tr_in.fPvers;
+}
+
+TABMntuTrackTr& TABMntuTrackTr::operator=(TABMntuTrackTr const& in)
+{
+   if(this!=&in){
+      fHitsN=in.fHitsN;
+      fChi2Red=in.fChi2Red;
+      fIsConverged=in.fIsConverged;
+      fPvers=in.fPvers;
+      fR0=in.fR0;
+      fEffFittedPlane=in.fEffFittedPlane;
+   }
+   return *this;
 }
 
 TVector3 TABMntuTrackTr::PointAtLocalZ(double zloc)
 {
-   TVector3 projected(Pvers.X()/Pvers.Z()*zloc+R0.X() ,Pvers.Y()/Pvers.Z()*zloc+R0.Y(), zloc);   
+   TVector3 projected(fPvers.X()/fPvers.Z()*zloc+fR0.X() ,fPvers.Y()/fPvers.Z()*zloc+fR0.Y(), zloc);
    return projected;
 }
 
@@ -68,8 +82,8 @@ TVector3 TABMntuTrackTr::PointAtLocalZ(double zloc)
 void TABMntuTrackTr::NewSet(TVectorD ftrackpar){
   Double_t transv=ftrackpar[2]*ftrackpar[2]+ftrackpar[3]*ftrackpar[3];
   if(transv<1.1){
-    Pvers.SetXYZ(ftrackpar[2], ftrackpar[3], sqrt(1.-transv));
-    R0.SetXYZ(ftrackpar[0], ftrackpar[1], 0.);
+    fPvers.SetXYZ(ftrackpar[2], ftrackpar[3], sqrt(1.-transv));
+    fR0.SetXYZ(ftrackpar[0], ftrackpar[1], 0.);
   }
   else{
     Error("Action()","TABMntuTrackTr::Set(vec)-> ERROR: pvers>1  px=%f py=%f px*px+py*py=%lf ",ftrackpar[2],ftrackpar[3],transv);
@@ -80,8 +94,8 @@ void TABMntuTrackTr::NewSet(TVectorD ftrackpar){
 
 
 void TABMntuTrackTr::PrintR0Pvers(){
-  cout<<"R0=("<<R0.X()<<", "<<R0.Y()<<" ,"<<R0.Z()<<")"<<endl;
-  cout<<"Pvers=("<<Pvers.X()<<", "<<Pvers.Y()<<" ,"<<Pvers.Z()<<")"<<endl;
+  cout<<"R0=("<<fR0.X()<<", "<<fR0.Y()<<" ,"<<fR0.Z()<<")"<<endl;
+  cout<<"Pvers=("<<fPvers.X()<<", "<<fPvers.Y()<<" ,"<<fPvers.Z()<<")"<<endl;
   return;
 }
 
