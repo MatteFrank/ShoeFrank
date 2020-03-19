712479e0532c5bb6ea90485251ab94abdb0cc9a0
Christian Finck <cfinck@iphc.cnrs.fr>
2020-03-19 15:30:54 +0100

    Put getter in const Disentangle data member

--- libs/src/TABMbase/TABMntuHit.hxx
+++ libs/src/TABMbase/TABMntuHit.hxx
@@ -24,73 +24,69 @@ class TABMntuHit : public TAGdata {
     TABMntuHit(Int_t iv, Int_t il, Int_t ic,Int_t id, Double_t r, Double_t t, Double_t s);
     virtual         ~TABMntuHit();
 
-    //~ void            SetData(Int_t id, Int_t iv, Int_t il, Int_t ic, Double_t x,    Double_t y, Double_t z, Double_t px, Double_t py, Double_t pz, Double_t r, Double_t t,Double_t s);
-    Int_t           Cell() const;
-    Int_t           Plane() const;
-    Int_t           View() const;
-    Double_t Dist() const; //return rdrift
+    Int_t    Cell()   const;
+    Int_t    Plane()  const;
+    Int_t    View()   const;
+    Double_t Dist()   const; //return frDrift
     Double_t Tdrift() const;
-    //~ Double_t Timmon() const;
-    
-
+   
     //Getters
     void SetAW();
-    Double_t GetChi2() {return ichi2;};
-    Double_t GetSigma() {return sigma;};
-    TVector3 GetA0() {return A0;};
-    TVector3 GetWvers() {return Wvers;};
-    Double_t GetResidual(){return residual;};
-    Int_t GetIsSelected() {return isSelected;};
-    Int_t GetIsFake() {return isFake;};
-    Double_t GetRealRdrift(){return realRdrift;};
-    Double_t GetRdriftSmear(){return fabs(realRdrift-rdrift);};
-    Int_t GetCellid(){return cellid;};
+    Double_t GetChi2()                  const { return fChi2;        }
+    Double_t GetSigma()                 const { return fSigma;       }
+    TVector3 GetA0()                    const { return fA0;          }
+    TVector3 GetWvers()                 const { return fWvers;       }
+    Double_t GetResidual()              const { return fResidual;    }
+    Int_t    GetIsSelected()            const { return fIsSelected;  }
+    Int_t    GetIsFake()                const { return fIsFake;      }
+    Double_t GetRealRdrift()            const { return fRealrDrift;  }
+    Double_t GetRdriftSmear()           const { return fabs(fRealrDrift-frDrift); }
+    Int_t    GetCellid()                const { return fiCellid;                  }
+    Int_t    GetMcIndex(Int_t index)    const { return fMCindex[index];           }
+    Int_t    GetMcTrackIdx(Int_t index) const { return fMcTrackIdx[index];        }
+    Int_t    GetMcTracksN()             const { return fMcTrackIdx.GetSize();     }
 
     //setters
-    void SetCellid(Int_t in_cellid) { cellid = in_cellid; return;};
-    void SetChi2(Double_t in_chi2) { ichi2 = in_chi2; return;};
-    void SetSigma(Double_t in_sigma) {sigma = in_sigma; return;};
-    void SetRdrift(Double_t in_rdrift){rdrift=in_rdrift;return;};
-    void SetTdrift(Double_t in_tdrift){tdrift=in_tdrift;return;};
-    void SetRealRdrift(Double_t in_rdrift){realRdrift=in_rdrift;return;};
-    void SetIsSelected(Int_t in_is){isSelected=in_is;return;};
-    void SetResidual(Double_t res_in){residual=res_in;return;};
-    void SetResidualSigma(Double_t res_in){residual=res_in*sigma;return;};
-    void SetA0(TVector3 a_in){A0=a_in;return;};
-    void SetWvers(TVector3 w_in){Wvers=w_in;return;};
-    void SetIsFake(Int_t in_fake){isFake=in_fake;return;};
+    void     SetfiCellid(Int_t in_fiCellid) { fiCellid = in_fiCellid;};
+    void     SetChi2(Double_t in_chi2) { fChi2 = in_chi2;};
+    void     SetSigma(Double_t in_sigma) {fSigma = in_sigma;};
+    void     SetRdrift(Double_t in_frDrift){frDrift=in_frDrift;};
+    void     SetTdrift(Double_t in_tdrift){ftDrift=in_tdrift;};
+    void     SetRealRdrift(Double_t in_frDrift){fRealrDrift=in_frDrift;};
+    void     SetIsSelected(Int_t in_is){fIsSelected=in_is;};
+    void     SetResidual(Double_t res_in){fResidual=res_in;};
+    void     SetResidualSigma(Double_t res_in){fResidual=res_in*fSigma;};
+    void     SetA0(TVector3 a_in){fA0=a_in;};
+    void     SetWvers(TVector3 w_in){fWvers=w_in;};
+    void     SetIsFake(Int_t in_fake){fIsFake=in_fake;};
    
-   Int_t           GetMcIndex(Int_t index)    const   { return fMCindex[index];       }
-   Int_t           GetMcTrackIdx(Int_t index) const   { return fMcTrackIdx[index];    }
-   Int_t           GetMcTracksN()             const   { return fMcTrackIdx.GetSize(); }
    
-   void            Clear(Option_t* option = "C");
-   void            AddMcTrackIdx(Int_t trackId, Int_t mcId = -1);
+   void      Clear(Option_t* option = "C");
+   void      AddMcTrackIdx(Int_t trackId, Int_t mcId = -1);
 
 
   ClassDef(TABMntuHit,1)
 
   private:
-    Int_t iview; //0=hit relevant for yz plane (sense wire on x direction), 1= hit relevant for xz plane (sense wire on y direction)   
-    Int_t ilayer; //0-5 number of bm plane   
-    Int_t icell;  //0-2 number of cell
-    Int_t cellid; //0-36 number that identify uniquely the cell
-    Double_t ichi2;
-    Double_t rdrift;
-    Double_t tdrift;//hit.time - T0 - irtime (irtime is the trigger time/start counter time) 
-    //~ Double_t timmon;//hit.time - irtime
-    Double_t sigma;//rdrift resolution
-    Double_t residual;//difference between rdrift and the fitted rdrift 
-    TVector3  A0;    //Position of the hit's wire
-    TVector3  Wvers; //direction of the hit's wire
-    
-    Int_t      isSelected;   //0=not set, 1=selected, -1=not selected 
+    Int_t     fiView;    //0=hit relevant for yz plane (sense wire on x direction), 1= hit relevant for xz plane (sense wire on y direction)
+    Int_t     fiLayer;   //0-5 number of bm plane
+    Int_t     fiCell;    //0-2 number of cell
+    Int_t     fiCellid;  //0-36 number that identify uniquely the cell
+    Double_t  fChi2;
+    Double_t  frDrift;
+    Double_t  ftDrift;   //hit.time - T0 - irtime (irtime is the trigger time/start counter time)
+
+    Double_t  fSigma;    //frDrift resolution
+    Double_t  fResidual; //difference between frDrift and the fitted frDrift
+    TVector3  fA0;       //Position of the hit's wire
+    TVector3  fWvers;    //direction of the hit's wire
+    Int_t     fIsSelected;   //0=not set, 1=selected, -1=not selected
     
     //MC parameters
-    Double_t  realRdrift;   //real rdrift, not smeared, only for MC 
-    Int_t      isFake;   //-1=not set, 0=primary hit, 1=secondary hit, 2=fake creator hit
-    TArrayI         fMCindex;                  // Id of the hit created in the simulation
-    TArrayI         fMcTrackIdx;                // Id of the track created in the simulation
+    Double_t  fRealrDrift;  //real frDrift, not smeared, only for MC
+    Int_t     fIsFake;       //-1=not set, 0=primary hit, 1=secondary hit, 2=fake creator hit
+    TArrayI   fMCindex;      // Id of the hit created in the simulation
+    TArrayI   fMcTrackIdx;   // Id of the track created in the simulation
 };
 
 
