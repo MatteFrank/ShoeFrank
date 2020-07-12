cf20a7c575e9aff371cf3b8c791a608bb85e6cf4
Giuseppe Battistoni <giuseppe.battistoni@mi.infn.it>
2020-07-12 16:05:12 +0200

    added source.f to ROUTINES for efficiency calculation

---
+++ Simulation/ROUTINES/PRO/source.f
@@ -0,0 +1,288 @@
+*$ CREATE SOURCE.FOR
+*COPY SOURCE
+*
+*=== source ===========================================================*
+*
+      SUBROUTINE SOURCE ( NOMORE )
+
+      INCLUDE '(DBLPRC)'
+      INCLUDE '(DIMPAR)'
+      INCLUDE '(IOUNIT)'
+*
+*----------------------------------------------------------------------*
+*                                                                      *
+*     Copyright (C) 1990-2020      by    Alfredo Ferrari & Paola Sala  *
+*     All Rights Reserved.                                             *
+*                                                                      *
+*                                                                      *
+*     New source for FLUKA9x-FLUKA20xy:                                *
+*                                                                      *
+*     Created on 07 January 1990   by    Alfredo Ferrari & Paola Sala  *
+*                                                Infn - Milan          *
+*                                                                      *
+*     Last change on  03-Jan-20    by             Paola Sala           *
+*                                                Infn - Milan          *
+*                                                                      *
+*  This is just an example of a possible user written source routine.  *
+*  note that the beam card still has some meaning - in the scoring the *
+*  maximum momentum used in deciding the binning is taken from the     *
+*  beam momentum.  Other beam card parameters are obsolete.            *
+*                                                                      *
+*       Output variables:                                              *
+*                                                                      *
+*              Nomore = if > 0 the run will be terminated              *
+*                                                                      *
+*----------------------------------------------------------------------*
+*
+      INCLUDE '(BEAMCM)'
+      INCLUDE '(FHEAVY)'
+      INCLUDE '(FLKSTK)'
+      INCLUDE '(IOIOCM)'
+      INCLUDE '(LTCLCM)'
+      INCLUDE '(PAPROP)'
+      INCLUDE '(SOURCM)'
+      INCLUDE '(SUMCOU)'
+*
+      LOGICAL LFIRST, LISNUT
+*
+      SAVE LFIRST
+      DATA LFIRST / .TRUE. /
+*  Statement function:
+      LISNUT (IJ) = INDEX ( PRNAME (IJ), 'NEUTRI' ) .GT. 0
+*======================================================================*
+*                                                                      *
+*                 BASIC VERSION                                        *
+*                                                                      *
+*======================================================================*
+      NOMORE = 0
+*  +-------------------------------------------------------------------*
+*  |  First call initializations:
+      IF ( LFIRST ) THEN
+*  |  *** The following 3 cards are mandatory ***
+         TKESUM = ZERZER
+         LFIRST = .FALSE.
+         LUSSRC = .TRUE.
+*  |  *** User initialization ***
+         EMIN = WHASOU(1)/1000.D+00
+         EMAX = WHASOU(2)/1000.D+00
+         WRITE(*,*) ' CosMin = ',COSMIN
+         IF (EMAX.LE.EMIN) CALL FLABRT( 'SOURCE' , ' EMIN <= EMAX ' )
+         WRITE(*,*) 'Flat Sampling, Emin/u = ',EMIN,' Emax/u = ',EMAX
+         IF (IJBEAM .EQ. -2) THEN
+            EMIN = EMIN*IPROA
+            EMAX = EMAX*IPROA
+         ENDIF         
+         WRITE(*,*) ' Emin tot = ',EMIN,' Emax tot = ',EMAX
+*     Distance between Target and TW"
+         DISTANCE = WHASOU(3)
+         WRITE(*,*) 'Distance from source to 1st layer = ',DISTANCE
+         IF (DISTANCE.LE.0) CALL FLABRT( 'SOURCE' , ' DIST <= ZERO ' )
+*     Diagonal of the TW
+         DIAGONAL = WHASOU(4)*SQRTWO
+         IF (DIAGONAL.LE.0) CALL FLABRT( 'SOURCE' , ' DIAG <= ZERO ' )
+         COSMIN = DISTANCE/SQRT( (DIAGONAL/TWOTWO)**2 + DISTANCE**2 )
+         WRITE(*,*) 'Cos Min = ',COSMIN
+*     Dimensions of Target
+         TGXMAX = WHASOU(5)/TWOTWO
+         TGYMAX = WHASOU(5)/TWOTWO
+         TGZMAX = WHASOU(6)/TWOTWO
+         TGXMIN = -TGXMAX
+         TGYMIN = -TGYMAX
+         TGZMIN = -TGZMAX
+         IF (TGXMAX.LE.0 .OR. TGZMAX.LE.0)
+     &        CALL FLABRT( 'SOURCE' , ' TgSize <= ZERO ' )
+      END IF
+*  |
+*  +-------------------------------------------------------------------*
+*  Push one source particle to the stack. Note that you could as well
+*  push many but this way we reserve a maximum amount of space in the
+*  stack for the secondaries to be generated
+*  Npflka is the stack counter: of course any time source is called it
+*  must be =0
+      NPFLKA = NPFLKA + 1
+*  Wt is the weight of the particle
+      WTFLK  (NPFLKA) = ONEONE
+      WEIPRI = WEIPRI + WTFLK (NPFLKA)
+*  Particle type (1=proton.....). Ijbeam is the type set by the BEAM
+*  card
+*  +-------------------------------------------------------------------*
+*  |  (Radioactive) isotope:
+      IF ( IJBEAM .EQ. -2 .AND. LRDBEA ) THEN
+         IARES  = IPROA
+         IZRES  = IPROZ
+         IISRES = IPROM
+         CALL STISBM ( IARES, IZRES, IISRES )
+         IJHION = IPROM  * 100000 + MOD ( IPROZ, 100 ) * 1000 + IPROA
+         IJHION = IJHION * 100    + KXHEAV
+         IONID  = IJHION
+         CALL DCDION ( IONID )
+         CALL SETION ( IONID )
+         EEXSTK (NPFLKA) = EXENRG (IONID)
+         TMNSTK (NPFLKA) = TMNLF  (IONID)
+         ILVSTK (NPFLKA) = IEXLVL (IONID)
+         LFRPHN (NPFLKA) = .FALSE.
+*  |
+*  +-------------------------------------------------------------------*
+*  |  Heavy ion:
+      ELSE IF ( IJBEAM .EQ. -2 ) THEN
+         IJHION = IPROM  * 100000 + MOD ( IPROZ, 100 ) * 1000 + IPROA
+         IJHION = IJHION * 100    + KXHEAV
+         IONID  = IJHION
+         CALL DCDION ( IONID )
+         CALL SETION ( IONID )
+         EEXSTK (NPFLKA) = EXENRG (IONID)
+         TMNSTK (NPFLKA) = TMNLF  (IONID)
+         ILVSTK (NPFLKA) = IEXLVL (IONID)
+         ILOFLK (NPFLKA) = IJHION
+*  |  Flag this is prompt radiation
+         LRADDC (NPFLKA) = .FALSE.
+*  |  Group number for "low" energy neutrons, set to 0 anyway
+         IGROUP (NPFLKA) = 0
+*  |  Parent radioactive isotope:
+         IRDAZM (NPFLKA) = 0
+*  |  Particle age (s)
+         AGESTK (NPFLKA) = +ZERZER
+*  |  Kinetic energy of the particle (GeV)
+*     Flat Sampling
+         TKEFLK (NPFLKA) = EMIN + ( EMAX - EMIN ) * FLRNDM(XDUMMY)
+*  |  Particle momentum
+*         PMOFLK (NPFLKA) = PBEAM
+         PMOFLK (NPFLKA) = SQRT ( TKEFLK (NPFLKA) * ( TKEFLK (NPFLKA)
+     &                          + TWOTWO * AM (IONID) ) )
+         LFRPHN (NPFLKA) = .FALSE.
+*  |
+*  +-------------------------------------------------------------------*
+*  |  Normal hadron:
+      ELSE
+         IONID = IJBEAM
+         EEXSTK (NPFLKA) = EXENRG (IJBEAM)
+         TMNSTK (NPFLKA) = TMNLF  (IJBEAM)
+         ILVSTK (NPFLKA) = IEXLVL (IJBEAM)
+         ILOFLK (NPFLKA) = IJBEAM
+*  |  Flag this is prompt radiation
+         LRADDC (NPFLKA) = .FALSE.
+*  |  Group number for "low" energy neutrons, set to 0 anyway
+         IGROUP (NPFLKA) = 0
+*  |  Parent radioactive isotope:
+         IRDAZM (NPFLKA) = 0
+*  |  Particle age (s)
+         AGESTK (NPFLKA) = +ZERZER
+*  |  Kinetic energy of the particle (GeV)
+*     Flat Sampling
+         TKEFLK (NPFLKA) = EMIN + ( EMAX - EMIN ) * FLRNDM(XDUMMY)
+*  |  Particle momentum
+*         PMOFLK (NPFLKA) = PBEAM
+         PMOFLK (NPFLKA) = SQRT ( TKEFLK (NPFLKA) * ( TKEFLK (NPFLKA)
+     &                          + TWOTWO * AM (IONID) ) )
+*  |  +----------------------------------------------------------------*
+*  |  |  Check if it is a neutrino, if so force the interaction
+*  |  |  (unless the relevant flag has been disabled)
+         IF ( LISNUT (IJBEAM) .AND. LNUFIN ) THEN
+            LFRPHN (NPFLKA) = .TRUE.
+*  |  |
+*  |  +----------------------------------------------------------------*
+*  |  |  Not a neutrino
+         ELSE
+            LFRPHN (NPFLKA) = .FALSE.
+         END IF
+*  |  |
+*  |  +----------------------------------------------------------------*
+      END IF
+*  |
+*  +-------------------------------------------------------------------*
+*  From this point .....
+*  Particle generation (1 for primaries)
+      LOFLK  (NPFLKA) = 1
+*  User dependent flag:
+      LOUSE  (NPFLKA) = 0
+*  No channeling:
+      KCHFLK (NPFLKA) = 0
+      ECRFLK (NPFLKA) = ZERZER
+*  Extra infos:
+      INFSTK (NPFLKA) = 0
+      LNFSTK (NPFLKA) = 0
+      ANFSTK (NPFLKA) = ZERZER
+*  Parent variables:
+      IPRSTK (NPFLKA) = 0
+      EKPSTK (NPFLKA) = ZERZER
+*  User dependent spare variables:
+      DO 100 ISPR = 1, MKBMX1
+         SPAREK (ISPR,NPFLKA) = ZERZER
+ 100  CONTINUE
+*  User dependent spare flags:
+      DO 200 ISPR = 1, MKBMX2
+         ISPARK (ISPR,NPFLKA) = 0
+ 200  CONTINUE
+*  Save the track number of the stack particle:
+      ISPARK (MKBMX2,NPFLKA) = NPFLKA
+      NPARMA = NPARMA + 1
+      NUMPAR (NPFLKA) = NPARMA
+      NEVENT (NPFLKA) = 0
+      DFNEAR (NPFLKA) = +ZERZER
+*  ... to this point: don't change anything
+      AKNSHR (NPFLKA) = -TWOTWO
+*  Cosines (tx,ty,tz)
+*     Uniform sampling in solid angle in the acceptance cone
+      CALL SFECFE(SINPH,COSPH)
+      COSTH = (ONEONE - COSMIN) * FLRNDM(XDUMMY) + COSMIN
+      SINTH = SQRT( (ONEONE-COSTH) * (ONEONE+COSTH) )
+      COSX = SINTH*COSPH
+      COSY = SINTH*SINPH
+      COSZ = COSTH
+      TXFLK  (NPFLKA) = COSX
+      TYFLK  (NPFLKA) = COSY
+      TZFLK  (NPFLKA) = COSZ
+*     TZFLK  (NPFLKA) = SQRT ( ONEONE - TXFLK (NPFLKA)**2
+*    &                       - TYFLK (NPFLKA)**2 )
+      WRITE(*,*) COSX,COSY,COSZ
+
+*     Polarization cosines:
+      TXPOL  (NPFLKA) = -TWOTWO
+      TYPOL  (NPFLKA) = +ZERZER
+      TZPOL  (NPFLKA) = +ZERZER
+*  Particle coordinates
+*     Uniform sampling in the target voluma
+      XFLK   (NPFLKA) = (TGXMAX-TGXMIN) * FLRNDM(XDUMMY) + TGXMIN
+      YFLK   (NPFLKA) = (TGYMAX-TGYMIN) * FLRNDM(XDUMMY) + TGYMIN
+      ZFLK   (NPFLKA) = (TGZMAX-TGZMIN) * FLRNDM(XDUMMY) + TGZMIN
+*  Calculate the total kinetic energy of the primaries: don't change
+*  +-------------------------------------------------------------------*
+*  |  (Radioactive) isotope:
+      IF ( IJBEAM .EQ. -2 .AND. LRDBEA ) THEN
+*  |
+*  +-------------------------------------------------------------------*
+*  |  Heavy ion:
+      ELSE IF ( ILOFLK (NPFLKA) .EQ. -2 .OR.
+     &          ILOFLK (NPFLKA) .GT. 100000 ) THEN
+         TKESUM = TKESUM + TKEFLK (NPFLKA) * WTFLK (NPFLKA)
+*  |
+*  +-------------------------------------------------------------------*
+*  |  Standard particle:
+      ELSE IF ( ILOFLK (NPFLKA) .NE. 0 ) THEN
+         TKESUM = TKESUM + ( TKEFLK (NPFLKA) + AMDISC (ILOFLK(NPFLKA)) )
+     &          * WTFLK (NPFLKA)
+*  |
+*  +-------------------------------------------------------------------*
+*  |
+      ELSE
+         TKESUM = TKESUM + TKEFLK (NPFLKA) * WTFLK (NPFLKA)
+      END IF
+*  |
+*  +-------------------------------------------------------------------*
+      RADDLY (NPFLKA) = ZERZER
+*  Here we ask for the region number of the hitting point.
+*     NREG (NPFLKA) = ...
+*  The following line makes the starting region search much more
+*  robust if particles are starting very close to a boundary:
+      CALL GEOCRS ( TXFLK (NPFLKA), TYFLK (NPFLKA), TZFLK (NPFLKA) )
+      CALL GEOREG ( XFLK  (NPFLKA), YFLK  (NPFLKA), ZFLK  (NPFLKA),
+     &              NRGFLK(NPFLKA), IDISC )
+*  Do not change these cards:
+      CALL GEOHSM ( NHSPNT (NPFLKA), 1, -11, MLATTC )
+      NLATTC (NPFLKA) = MLATTC
+      CMPATH (NPFLKA) = ZERZER
+      CALL SOEVSV
+      RETURN
+*=== End of subroutine Source =========================================*
+      END
+
