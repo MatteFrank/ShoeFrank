081bfdc9fb5694aae6230c76aa13c3461937a46a
Christian Finck <cfinck@iphc.cnrs.fr>
2020-07-27 11:13:02 +0200

    Correct folder for T0 calibration

--- libs/src/TAGfoot/BaseReco.cxx
+++ libs/src/TAGfoot/BaseReco.cxx
@@ -343,7 +343,7 @@ void BaseReco::ReadParFiles()
       TABMparCon* parConf = (TABMparCon*)fpParConfBm->Object();
       parFileName = "./config/TABMdetector.cfg";
       parConf->FromFile(parFileName.Data());
-      parFileName = Form("./config/%sTABM_T0_Calibration.cal", fExpName.Data());
+      parFileName = Form("./calib/%sTABM_T0_Calibration.cal", fExpName.Data());
       parConf->loadT0s(parFileName);
             
       fpParMapBm = new TAGparaDsc("bmMap", new TABMparMap());
