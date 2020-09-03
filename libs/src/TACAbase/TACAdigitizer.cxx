9045652d85bf29fcaa47ff6f2a79eb5e2c877953
Christian Finck <cfinck@iphc.cnrs.fr>
2020-09-03 10:57:31 +0200

    Add energy resolution (taken from Calo branch)

--- libs/src/TACAbase/TACAdigitizer.cxx
+++ libs/src/TACAbase/TACAdigitizer.cxx
@@ -3,18 +3,25 @@
 #include "TF1.h"
 #include "TMath.h"
 
+#include "TAGgeoTrafo.hxx"
 #include "TACAntuRaw.hxx"
 #include "TACAparGeo.hxx"
 #include "TACAdigitizer.hxx"
 
 
-Float_t TACAdigitizer::fgThreshold = 10; // MeV
+Float_t TACAdigitizer::fgThreshold = 25; // MeV
 
 // --------------------------------------------------------------------------------------
 TACAdigitizer::TACAdigitizer(TACAntuRaw* pNtuRaw)
  : TAGbaseDigitizer(),
    fpNtuRaw(pNtuRaw),
-   fGain(10/3.)
+   fGain(10/3.),
+   fResPar0(1.21),
+   fResErrPar0(7e-3),
+   fResPar1(-1.37),
+   fResErrPar1(0.58),
+   fResPar2(5.07e-2),
+   fResErrPar2(2.12e-4)
 {
    SetFunctions();
    SetParFunction();
@@ -25,13 +32,16 @@ void  TACAdigitizer::SetFunctions()
 {
    // compute birks law for a given Edep
    fFuncBirks = new TF1("PhotonsN", this, &TACAdigitizer::RecPhotonsN, 0, 20, 3, "TACAdigitizer", "RecPhotonsN");
+   
+   fDeResE    = new TF1("ResEnergy", this, &TACAdigitizer::ResEnergy, 50, 5000, 3, "TACAdigitizer", "ResEnergy");
 }
 
 // --------------------------------------------------------------------------------------
 void  TACAdigitizer::SetParFunction()
 {
    // photons yield (n/MeV), kB: birks (mm/MeV), collection efficiency (0.4%) for BGO
-   fFuncBirks->SetParameters(9000, 3.679e-3, 1./240.); 
+   fFuncBirks->SetParameters(9000, 3.679e-3, 1./240.);
+   fDeResE   ->SetParameters(fResPar0, fResPar1, fResPar2);
 }
 
 // --------------------------------------------------------------------------------------
@@ -50,18 +60,39 @@ Double_t TACAdigitizer::RecPhotonsN(Double_t* x, Double_t* par)
 }
 
 //___________________________________________________________________________________________
+Double_t TACAdigitizer::ResEnergy(Double_t* x, Double_t* par)
+{
+   Float_t energy = x[0];
+   Float_t res = (par[0]*par[0]/energy + par[1]*par[1]/(energy*energy) + par[2]*par[2]);
+   
+   return res;
+}
+//___________________________________________________________________________________________
 Float_t TACAdigitizer::GetPhotonsN(Float_t /*X*/, Float_t /*Y*/, Float_t edep)
 {
    return fFuncBirks->Eval(edep);
 }
 
 //___________________________________________________________________________________________
+Float_t TACAdigitizer::GetResEnergy(Float_t edep)
+{
+   return fDeResE->Eval(edep)*edep;
+}
+
+//___________________________________________________________________________________________
 Bool_t TACAdigitizer::Process(Double_t edep, Double_t x0, Double_t y0, Double_t /*zin*/, Double_t /*zout*/, Double_t time, Int_t id, Int_t /*Z*/)
 {
-   if (edep < fgThreshold) return true;
+   if (edep < fgThreshold) {
+      fCurrentHit = 0x0;
+      return true;
+   }
    
-   Float_t photonsN = edep; //GetPhotonsN(x0, y0, edep)*fGain;
+   Float_t resEnergy = GetResEnergy(edep);
+   edep += gRandom->Gaus(0, resEnergy);
+   edep = 1.252*edep - 28.42; // rough calibration
    
+   Float_t photonsN = edep; //GetPhotonsN(x0, y0, edep)*fGain; // skip Birks law for the moment
+
    if (fMap[id] == 0) {
       fCurrentHit = (TACAntuHit*)fpNtuRaw->NewHit(id, photonsN, time);
       fMap[id] = fCurrentHit;
--- libs/src/TACAbase/TACAdigitizer.hxx
+++ libs/src/TACAbase/TACAdigitizer.hxx
@@ -25,8 +25,12 @@ public:
    Float_t        GetPhotonsN(Float_t X, Float_t Y, Float_t edep);
    Double_t       RecPhotonsN(Double_t* x, Double_t* par);
  
+   Float_t        GetResEnergy(Float_t edep);
+   Double_t       ResEnergy(Double_t* x, Double_t* par);
+
    TF1*           GetFuncBirks() const { return fFuncBirks; }
-   
+   TF1*           GetFuncResE()  const { return fDeResE; }
+
    void           SetGain(Float_t g)   { fGain = g;          }
 
    TACAntuHit*    GetCurrentHit()      { return fCurrentHit; }
@@ -39,9 +43,17 @@ public:
 private:
    TACAntuRaw*   fpNtuRaw;
    TF1*          fFuncBirks;
+   TF1*          fDeResE;
    Float_t       fGain;
-   TACAntuHit*   fCurrentHit;
+   
+   Float_t       fResPar0;
+   Float_t       fResErrPar0;
+   Float_t       fResPar1;
+   Float_t       fResErrPar1;
+   Float_t       fResPar2;
+   Float_t       fResErrPar2;
 
+   TACAntuHit*   fCurrentHit;
    map<int, TACAntuHit*> fMap; //! map for pilepup
 
 private:
