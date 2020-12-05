e16cd4ca39bad7a80686d10da81855931d804f41
Christian Finck <cfinck@iphc.cnrs.fr>
2020-12-05 15:11:42 +0100

    Update class to be coherent with GlobalToeReco

--- libs/src/TAGfoot/GlobalReco.cxx
+++ libs/src/TAGfoot/GlobalReco.cxx
@@ -5,12 +5,73 @@
 ClassImp(GlobalReco)
 
 //__________________________________________________________
-GlobalReco::GlobalReco(TString expName, Int_t runNumber, TString fileNameIn, TString fileNameout)
- : LocalReco(expName, runNumber, fileNameIn, fileNameout)
+GlobalReco::GlobalReco(TString expName, Int_t runNumber, TString fileNameIn, TString fileNameout, Bool_t isMC)
+: BaseReco(expName, runNumber, fileNameIn, fileNameout)
 {
+  GlobalPar::GetPar()->EnableLocalReco();
+  fFlagMC = isMC;
 }
 
 //__________________________________________________________
 GlobalReco::~GlobalReco()
 {
+}
+
+//__________________________________________________________
+void GlobalReco::OpenFileIn()
+{
+  fActEvtReader->Open(GetName());
+}
+
+//__________________________________________________________
+void GlobalReco::CloseFileIn()
+{
+  fActEvtReader->Close();
+}
+
+//__________________________________________________________
+void GlobalReco::CreateRawAction()
+{
+}
+
+// --------------------------------------------------------------------------------------
+void GlobalReco::SetRunNumber()
+{
+  if (fRunNumber != -1) { // if set from outside return, else take from name
+    gTAGroot->SetRunNumber(fRunNumber);
+    return;
+  }
+  
+  // Done by hand shoud be given by DAQ header
+  TString name = GetName();
+  if (name.IsNull()) return;
+  
+  // protection about file name starting with .
+  if (name[0] == '.')
+    name.Remove(0,1);
+  
+  if (fFlagMC) {
+    // assuming name XXX_run.root
+    Int_t pos1   = name.Last('_');
+    Int_t len    = name.Length();
+    
+    TString tmp1 = name(pos1+1, len);
+    Int_t pos2   = tmp1.First(".");
+    TString tmp  = tmp1(0, pos2);
+    fRunNumber = tmp.Atoi();
+    
+  } else {
+    // assuming XXX.run.XXX.dat
+    Int_t pos1   = name.First(".");
+    Int_t len    = name.Length();
+    
+    TString tmp1 = name(pos1+1, len);
+    Int_t pos2   = tmp1.First(".");
+    TString tmp  = tmp1(0, pos2);
+    fRunNumber = tmp.Atoi();
+  }
+  
+  Warning("SetRunNumber()", "Run number not set !, taking number from file: %d", fRunNumber);
+  
+  gTAGroot->SetRunNumber(fRunNumber);
 }
--- libs/src/TAGfoot/GlobalReco.hxx
+++ libs/src/TAGfoot/GlobalReco.hxx
@@ -3,17 +3,30 @@
 #define _GlobalReco_HXX_
 
 #include "TString.h"
-#include "LocalReco.hxx"
+#include "BaseReco.hxx"
 
 
-class GlobalReco : public LocalReco 
+class GlobalReco : public BaseReco
 {
 public:
    //! default constructor
-   GlobalReco(TString expName, Int_t runNumber = -1, TString fileNameIn = "", TString fileNameout = "");
+   GlobalReco(TString expName, Int_t runNumber = -1, TString fileNameIn = "", TString fileNameout = "", Bool_t isMc = true);
    
    virtual ~GlobalReco();
-   
+  
+  //! Create raw data action
+  virtual void CreateRawAction();
+  
+  //! Open File
+  virtual void OpenFileIn();
+  
+  //! Close File in
+  virtual void CloseFileIn();
+  
+  //! Set run number
+  void   SetRunNumber();
+
+  
    ClassDef(GlobalReco, 0); 
 };
 
