78d548b9d5ae6f27fca764c6610c2627a3bfb9dd
Christian Finck <cfinck@iphc.cnrs.fr>
2020-12-13 10:45:49 +0100

    Correct formula for de/dx

--- libs/src/TADIbase/TADItrackEmProperties.cxx
+++ libs/src/TADIbase/TADItrackEmProperties.cxx
@@ -146,7 +146,7 @@ Float_t TADItrackEmProperties::GetdEdX(const TString& mat, Double_t beta,  Doubl
    
    Double_t I       = GetMeanExcitationEnergy(mat);
 
-   Double_t Q       = K*rho*(Zmed/Amed)*zBeam*zBeam;      //Kroz2Z/A
+   Double_t Q       = K*zBeam*zBeam*Zmed/Amed;      //Kz^2xZ/A
    Double_t me      = TAGgeoTrafo::GetElectronMassMeV();
    
    // calculating the gamma and Tmax
@@ -157,7 +157,7 @@ Float_t TADItrackEmProperties::GetdEdX(const TString& mat, Double_t beta,  Doubl
    Double_t gamma   = TMath::Sqrt(1/(1-beta2));
    Double_t gamma2  = gamma*gamma;
    Double_t Tmax    = 2*gamma2*beta2*me/(1+(2*gamma*(me/mass))+me*me/(mass*mass));
-   Double_t logar   = (0.5*TMath::Log(2*me*beta2*gamma2*Tmax/(I*I))-beta2);
+   Double_t logar   = 0.5*TMath::Log(2*me*beta2*gamma2*Tmax/(I*I)) - beta2;
    Double_t dEdX    = Q/beta2*logar;
    
    return dEdX;
