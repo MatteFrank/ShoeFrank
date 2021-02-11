8ce3fc448627b7f7c4f3609058f4646607df1686
Christian Finck <cfinck@iphc.cnrs.fr>
2021-02-11 16:41:55 +0100

    Add example to read from Shoe structure MC root file

--- Reconstruction/level0/macros/DisplayMcFOOT.C
+++ Reconstruction/level0/macros/DisplayMcFOOT.C
@@ -9,13 +9,16 @@
 #include "TAFOeventDisplayMC.hxx"
 #endif
 
-void DisplayMcFOOT(TString file = "12C_C_200_1.root", TString expName = "12C_200", Int_t runNumber = 1)
+//void DisplayMcFOOT(TString file = "12C_C_200shoe.root", TString expName = "12C_200", Int_t runNumber = 1, Int_t type = 2)
+void DisplayMcFOOT(TString file = "12C_C_200_1.root", TString expName = "12C_200", Int_t runNumber = 1, Int_t type = 1)
 {
+  // type = 1: Fluka MC structure
+  // type = 2: Shoe MC structure
    TAFObaseEventDisplay::SetVtxTrackingAlgo('F');
 //   TAFObaseEventDisplay::EnableItrTracking();
 //   TAFObaseEventDisplay::DisableTracking();
    
-   TAFOeventDisplayMC::Instance(expName)->ShowDisplay(file, runNumber);
+   TAFOeventDisplayMC::Instance(expName, type)->ShowDisplay(file, runNumber);
 }
 
 
