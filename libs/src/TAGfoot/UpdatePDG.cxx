807a25b1a32bd2eca232f56c83325a7eb6d08f8a
Christian Finck <cfinck@iphc.cnrs.fr>
2021-03-27 14:56:04 +0100

    Update from master branch

--- libs/src/TAGfoot/UpdatePDG.cxx
+++ libs/src/TAGfoot/UpdatePDG.cxx
@@ -2,14 +2,8 @@
 
 #include "UpdatePDG.hxx"
 
-
-
-
 // Global static pointer used to ensure a single instance of the class.
 UpdatePDG* UpdatePDG::m_pInstance = NULL;
-
-
-
 
 
 //----------------------------------------------------------------------------------------------------
@@ -26,9 +20,6 @@ UpdatePDG* UpdatePDG::Instance()  {
 }
 
 
-
-
-
 //----------------------------------------------------------------------------------------------------
 UpdatePDG* UpdatePDG::GetPDG()  {
   
@@ -39,19 +30,12 @@ UpdatePDG* UpdatePDG::GetPDG()  {
 }
 
 
-
-
-
 //----------------------------------------------------------------------------------------------------
 UpdatePDG::UpdatePDG() {
   
   MakePdgDatabase();
   
 }
-
-
-
-
 
 
 //----------------------------------------------------------------------------------------------------
@@ -61,7 +45,7 @@ void UpdatePDG::MakePdgDatabase() {
   // clean the particle datatbase. Important!
   TDatabasePDG::Instance()->~TDatabasePDG();
   
-  int nNewParticles = 17;
+  int nNewParticles = 20;
   int pdgCode = 66666600;
   // particle name
   vector<string> nameVector 		 = { 	"C11", "C12", "C13", "C14", 
@@ -69,7 +53,7 @@ void UpdatePDG::MakePdgDatabase() {
 						"B7", "B9", "B10",
 						"Be10", "Be11",
 						"N14", "N15",
-						"Alpha", "H",
+						"Alpha", "He3", "H", "H2", "H3",
 						"O15", "O16" };
   if ( (int)nameVector.size() != nNewParticles ) 	{
     cout << "ERROR::UpdatePDG::MakePdgDatabase  -->  particle collection name size not match "<< nameVector.size() <<endl;
@@ -82,7 +66,7 @@ void UpdatePDG::MakePdgDatabase() {
 			6.563, 8.357, 9.293,
 			9.32444, 10.2525,
 			13.1, 13.97,
-			4, 1,
+			4, 3.016, 1, 2.014, 3.016,
 			14, 14.88 };
   
   // particle cherge x3
@@ -91,42 +75,8 @@ void UpdatePDG::MakePdgDatabase() {
 				12, 12, 12,
 				15, 15, 
 				21, 21,
-				6, 3,
+				6, 6, 3, 3, 3,
 				24, 24  };
-  
-  
-  // int nNewParticles = 18;
-  // int pdgCode = 66666600;
-  // // particle name
-  // vector<string> nameVector 		 = { 	"C10", "C11", "C12", 
-  // 										"Li6", "Li7",
-  // 										"B7", "B8", "B9",
-  // 										"Be9", "Be10", "Be11",
-  // 										"N12", "N13", "N14",
-  // 										"Alpha", "H",
-  // 										"O15", "O16" };
-  // if ( (int)nameVector.size() != nNewParticles ) 	{
-  // 	cout << "ERROR::UpdatePDG::MakePdgDatabase  -->  particle collection name size not match "<< nameVector.size() <<endl;
-  // 	exit(0);
-  // }
-  
-  // // particle mass
-  // double massV [] = { 	10.254, 11.1749, 12.1095, 
-  // 									6.53383, 7,
-  // 									7, 8, 9.3255,
-  // 									9.32444, 10.2525, 11,
-  // 									12.1112, 13, 14,
-  // 									4, 1,
-  // 									15, 16 };
-  
-  // // particle cherge x3
-  // double chargeV [] = { 	18, 18, 18, 
-  // 									9, 9,
-  // 									12, 12, 12,
-  // 									15, 15, 15,
-  // 									21, 21, 21,
-  // 									6, 3,
-  // 									24, 24  };
   
   // check that every particle defined in the parameter file is defined in nameVector
   for ( unsigned int i=0; i<GlobalPar::GetPar()->MCParticles().size(); i++) {
