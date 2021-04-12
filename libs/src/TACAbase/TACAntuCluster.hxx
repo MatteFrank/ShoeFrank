4107a23842019e068a8769a1f5908d970344490f
Christian Finck <cfinck@iphc.cnrs.fr>
2021-04-12 16:58:40 +0200

    Return correct member

--- libs/src/TACAbase/TACAntuCluster.hxx
+++ libs/src/TACAbase/TACAntuCluster.hxx
@@ -53,7 +53,7 @@ public:
    //! Get position in global tracker frame
    const TVector3&     GetPositionG()                  const { return fPositionG ;     }
    //! Get position in global tracker frame
-   const TVector3&     GetPosErrorG()                  const { return fPositionG ;     }
+   const TVector3&     GetPosErrorG()                  const { return fPosErrorG ;     }
 
    //! Get validity
    Bool_t             IsValid()                        const { return fIsValid;        }
