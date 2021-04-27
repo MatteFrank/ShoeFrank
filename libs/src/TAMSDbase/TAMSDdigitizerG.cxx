cb2ebbb7f9697b2e918495b5941853a13512c149
Christian Finck <cfinck@iphc.cnrs.fr>
2021-04-27 14:42:16 +0200

    Add new digitiser for MSD (only template)

---
+++ libs/src/TAMSDbase/TAMSDdigitizerG.cxx
@@ -0,0 +1,235 @@
+#include "TAMSDdigitizerG.hxx"
+#include "TAMSDparGeo.hxx"
+#include "TAGgeoTrafo.hxx"
+
+#include "TAGrecoManager.hxx"
+
+using namespace std;
+
+Float_t TAMSDdigitizerG::fgkThres         = 0.1;
+Float_t TAMSDdigitizerG::fgkFWTH          = 2*TMath::Sqrt(2*TMath::Log(1./fgkThres));
+Bool_t  TAMSDdigitizerG::fgSmearFlag      = true;
+Float_t TAMSDdigitizerG::fgDefSmearPos    = 25;    // in micron
+Float_t TAMSDdigitizerG::fgkPairCreation  = 3.6e-3; // keV
+Float_t TAMSDdigitizerG::fgkFanoFactor    = 0.115;
+Float_t TAMSDdigitizerG::fgkNormFactor    = TMath::Sqrt(2*TMath::Pi());
+
+
+// --------------------------------------------------------------------------------------
+TAMSDdigitizerG::TAMSDdigitizerG(TAMSDparGeo* parGeo)
+ : TAGbaseDigitizer(),
+   fDe0Par(-8.7),
+   fDe0ParErr(0.2),
+   fRsPar(1.39),
+   fRsParErr(0.03),
+   fThresPar(503),
+   fThresParErr(66),
+   fLinPar(1.59e-2),
+   fLinParErr(0.07e-2),
+   fClusterHeight(0.),
+   fLogHeightPar(665),
+   fLogHeightParErr(46),
+   fCstHeightPar(2655),
+   fCstHeightParErr(250),
+   fZcstHeightPar(0.935),
+   fZgainHeightPar(0.0325),
+   fClusterWidth(0.),
+   fCstWidthPar(2.2),
+   fCstWidthParErr(0.1),
+   fTotConversion(0.73),
+   fTotExponent(0.6),
+   fTotThres(0.5)
+{
+   SetFunctions();
+   
+   fPitch   = fpParGeo->GetPitchX()*TAGgeoTrafo::CmToMu();
+   fStripsN = fpParGeo->GetStripsN();
+   
+}
+
+// --------------------------------------------------------------------------------------
+TAMSDdigitizerG::~TAMSDdigitizerG()
+{
+   delete fFuncClusterDis;
+}
+
+//------------------------------------------+-----------------------------------
+//! fill pixel signal
+Bool_t TAMSDdigitizerG::Process( Double_t edep, Double_t x0, Double_t y0, Double_t zin, Double_t zout, Double_t /*time*/, Int_t sensorId, Int_t Z, Double_t /*px0*/, Double_t /*py0*/, Double_t /*pz0*/)
+{
+   x0 *= TAGgeoTrafo::CmToMu();
+   Double_t  smear = 0;
+   
+   Double_t deltaE = edep*TAGgeoTrafo::GevToKev();
+   Float_t electronsN = deltaE/fgkPairCreation;
+   
+   // cluster height
+   smear = gRandom->Gaus(0, fCstHeightParErr);
+   fFuncClusterHeight->SetParameter(0, fCstHeightPar+smear);
+   
+   smear = gRandom->Gaus(0, fLogHeightParErr);
+   fFuncClusterHeight->SetParameter(1, fLogHeightPar+smear);
+   
+   smear = 0;
+   fFuncClusterHeight->SetParameter(2, fZcstHeightPar+smear);
+   
+   smear = 0;
+   fFuncClusterHeight->SetParameter(3, fZgainHeightPar+smear);
+   
+   fClusterHeight = fFuncClusterHeight->Eval(deltaE, Z);
+   if (fClusterHeight <= 1) fClusterHeight = 1.;
+   
+   // cluster width
+   smear = gRandom->Gaus(0, fCstWidthParErr);
+   fFuncClusterWidth->SetParameter(0, fCstWidthPar+smear);
+   
+   fClusterWidth = fFuncClusterWidth->Eval(deltaE, Z);
+   if (fClusterWidth <= 0) fClusterWidth = 0.8;
+
+   if(FootDebugLevel(1)) {
+      printf("\nnext hit:\n");
+      printf("eloss %6.1f pixels %d\n", deltaE, fStrips);
+   }
+   
+   if (fgSmearFlag) {
+      Float_t dx = gRandom->Gaus(0, fgDefSmearPos);
+      x0 += dx;
+   }
+   
+   return MakeCluster(x0, y0, zin, zout, sensorId);
+}
+
+
+// --------------------------------------------------------------------------------------
+Bool_t TAMSDdigitizerG::MakeCluster(Double_t x0, Double_t y0, Double_t /*zin*/, Double_t /*zout*/, Int_t sensorId)
+{
+   // std array
+   fMap.clear();
+   
+   Int_t view  = fpParGeo->GetSensorPar(sensorId).TypeIdx;
+   Float_t pos = 0.;
+   
+   if (view == 0)
+      pos = x0;
+   else
+      pos = y0;
+   
+   fMap.clear();
+   
+   if (TMath::Abs(pos) > fPitch*fStripsN/2.) return false;
+   
+   Int_t strip = GetStrip(pos);
+   
+   if(FootDebugLevel(1))
+      printf("%d %d\n", fStrips, strip);
+   
+   // choose the cluster version
+   Int_t reg     = GetRegion(pos);
+   Float_t sigma = fClusterWidth;
+   if (reg == 1) reg = 0;
+
+   if(FootDebugLevel(1))
+      printf("%g\n", fClusterWidth);
+   
+   Float_t height = fClusterHeight;
+   Float_t sig    = fgkFWTH*sigma;
+   Int_t xmin     = TMath::Nint(strip-sig);
+   Int_t xmax     = TMath::Nint(strip+sig);
+
+   fFuncClusterDis->SetParameters(height, strip+reg/10., sigma);
+   fFuncClusterDis->SetRange(xmin, xmax);
+   
+   for (Int_t x = xmin;  x <= xmax; ++x) {
+      Float_t value = fFuncClusterDis->Eval(x);
+      Float_t err = TMath::Sqrt(value*fgkFanoFactor);
+      value += gRandom->Uniform(err);
+      
+      if (value > height*fgkThres)
+         fMap[x] = GetAdcValue(value);
+   }
+   
+   return true;
+}
+
+// --------------------------------------------------------------------------------------
+void TAMSDdigitizerG::SetFunctions()
+{
+   fFuncClusterDis = new TF1("ClusterDis", "gaus");
+   
+   // compute cluster width/height for a given Edep, x and y
+   fFuncClusterHeight = new TF1("ClusterHeight", this, &TAMSDdigitizerG::FuncClusterHeight, 0, 2000, 4, "TAMSDdigitizerG", "FuncClusterHeight");
+   fFuncClusterWidth  = new TF1("ClusterWidth",  this, &TAMSDdigitizerG::FuncClusterWidth,  0, 2000, 4, "TAMSDdigitizerG", "FuncClusterWidth");
+}
+
+// --------------------------------------------------------------------------------------
+Int_t TAMSDdigitizerG::GetAdcValue(Float_t charge)
+{
+   return int(fFuncToDigital->Eval(charge)+0.5);
+}
+
+// --------------------------------------------------------------------------------------
+Double_t TAMSDdigitizerG::FuncClusterHeight(Double_t* x, Double_t* par)
+{
+   Float_t de = x[0];
+   Float_t z  = x[1];
+   
+   Float_t f = (par[0]+par[1]*TMath::Log(de))*(par[2] + par[3]*z);
+   
+   return f;
+}
+
+// --------------------------------------------------------------------------------------
+Double_t TAMSDdigitizerG::FuncClusterWidth(Double_t* x, Double_t* par)
+{
+   Float_t xx = x[0];
+   Float_t f = TMath::PoissonI(xx, par[0]);
+   
+   return f;
+}
+
+//_____________________________________________________________________________
+Int_t TAMSDdigitizerG::GetStrip(Float_t pos) const
+{
+   Float_t posmin = -fStripsN*fPitch/2.;
+   
+   if (pos < posmin || pos > -posmin) {
+      if(FootDebugLevel(1))
+         Warning("GetStrip()", "Value of position: %f out of range +/- %f\n", pos, posmin);
+      return -1;
+   }
+   
+   Int_t strip = floor((pos-posmin)/fPitch);
+   
+   return strip;
+}
+
+//_____________________________________________________________________________
+Float_t TAMSDdigitizerG::GetRemainder(Float_t pos) const
+{
+   Float_t posmin = -fStripsN*fPitch/2.;
+   
+   if (pos < posmin || pos > -posmin) {
+      if(FootDebugLevel(1))
+         Warning("GetRemainder()", "Value of position: %f out of range +/- %f\n", pos, posmin);
+      return -1;
+   }
+   
+   Float_t line = 1 -((pos-posmin)/fPitch - floor((pos-posmin)/fPitch));
+   
+   return line;
+}
+
+//_____________________________________________________________________________
+Int_t TAMSDdigitizerG::GetRegion(Float_t pos) const
+{
+   Float_t rem = GetRemainder(pos);
+   
+   if (rem < 0.33)
+      return 0;
+   else if (rem > 0.33 && rem < 0.66)
+      return 5;
+   else if (rem > 0.66)
+      return 10;
+   else
+      return -99;
+}
---
+++ libs/src/TAMSDbase/TAMSDdigitizerG.hxx
@@ -0,0 +1,121 @@
+#ifndef _TAMSDdigitizerG_HXX
+#define _TAMSDdigitizerG_HXX
+
+#include <map>
+
+#include "Riostream.h"
+#include "TF1.h"
+#include "TH2F.h"
+#include "TMath.h"
+#include "TObject.h"
+#include "TRandom3.h"
+#include "TString.h"
+
+#include "TAGbaseDigitizer.hxx"
+
+// --------------------------------------------------------------------------------------
+class TAMSDparGeo;
+class TAMSDdigitizerG : public TAGbaseDigitizer {
+   
+   // Class to digitize the energy into pixel based on given patterns
+public:
+   TAMSDdigitizerG(TAMSDparGeo* parGeo);
+   virtual ~TAMSDdigitizerG();
+   
+   virtual       Bool_t Process(Double_t edep, Double_t x0, Double_t y0, Double_t zin = 0, Double_t zout = 0, Double_t time = 0,
+                                Int_t sensorId = 0, Int_t Z = -99, Double_t px0 = 0, Double_t py0 = 0, Double_t pz0 = 0);
+   
+   Bool_t        MakeCluster(Double_t x0, Double_t y0, Double_t zin, Double_t zout, Int_t sensorId);
+   
+   Double_t      FuncClusterHeight(Double_t* x, Double_t* par);
+   Double_t      FuncClusterWidth(Double_t* x, Double_t* par);
+   
+   //! Get strip from x position
+   Int_t         GetStrip(Float_t x)     const;
+   
+   //! Get remainder of strip
+   Float_t       GetRemainder(Float_t x) const;
+   
+   //! Get strip region
+   Int_t         GetRegion(Float_t x)    const;
+   
+   void          SetRsPar(Double_t par)           { fRsPar = par;              }
+   void          SetRsParErr(Double_t par)        { fRsParErr = par;           }
+   void          SetThresPar(Double_t par)        { fThresPar = par;           }
+   void          SetThresParErr(Double_t par)     { fThresParErr = par;        }
+   
+   Float_t       GetClusterHeight() const         { return fClusterHeight;     }
+   void          SetLogHeightPar(Double_t par)    { fLogHeightPar = par;       }
+   void          SetLogHeightParErr(Double_t par) { fLogHeightParErr = par;    }
+   void          SetCstHeightPar(Double_t par)    { fCstHeightPar = par;       }
+   void          SetCstHeightParErr(Double_t par) { fCstHeightParErr = par;    }
+   
+   Float_t       GetClusterWidth()  const         { return fClusterWidth;      }
+   void          SetCstWidthPar(Double_t par)     { fCstWidthPar = par;        }
+   void          SetCstWidthParErr(Double_t par)  { fCstWidthParErr = par;     }
+   
+   Float_t       GetStripsN()       const         { return fStripsN;           }
+   Float_t       GetPitch ()        const         { return fPitch;             }
+   
+   TF1*          GetFuncClusterHeight()           { return fFuncClusterHeight; }
+   TF1*          GetFuncClusterWidth()            { return fFuncClusterWidth;  }
+
+   //! Get Map
+   std::map<int, double>  GetMap() const         { return fMap;               }
+   
+private:
+   TAMSDparGeo* fpParGeo;
+   
+   TF1*         fFuncClusterDis;    // cluster distribution function
+   TF1*         fFuncClusterHeight; // cluster charge height function
+   TF1*         fFuncClusterWidth;  // cluster charge width function
+   TF1*         fFuncToDigital;     // time over threshold digital function
+   
+   std::map<int, double> fMap;   // map of found pixels
+   
+   Double_t     fDe0Par;          // parameter shift in edep for the cluster size function
+   Double_t     fDe0ParErr;       // parameter shift in edep for the cluster size function
+   Double_t     fRsPar;           // parameter r_s for the cluster size function
+   Double_t     fRsParErr;        // parameter r_s for the cluster size function
+   Double_t     fThresPar;        // parameter threshold for cluster size function
+   Double_t     fThresParErr;     // parameter threshold for cluster size function
+   Double_t     fLinPar;          // parameter linear for cluster size function
+   Double_t     fLinParErr;       // parameter linear for cluster size function
+   
+   Double_t     fClusterHeight;   // height of the cluster for a given eloss
+   Double_t     fLogHeightPar;    // Log parameter for the cluster height function
+   Double_t     fLogHeightParErr; // Log parameter for the cluster height function
+   Double_t     fCstHeightPar;    // constant parameter for cluster height function
+   Double_t     fCstHeightParErr; // constant parameter for cluster height function
+   Double_t     fZcstHeightPar;   // z constant parameter for the cluster height function
+   Double_t     fZgainHeightPar;  // z gain parameter for the cluster height function
+   
+   Double_t     fClusterWidth;    // width of the cluster for a given eloss
+   Double_t     fCstWidthPar;     // constant parameter for cluster width function
+   Double_t     fCstWidthParErr;  // constant parameter for cluster width function
+   
+   Double_t     fTotConversion;   // conversion factor for Time over threshold
+   Double_t     fTotExponent;     // exponent factor for Time over threshold
+   Double_t     fTotThres;        // threshold factor for Time over threshold
+   
+   Int_t        fStripsN;         // number of strips
+   Int_t        fStrips;          // current number of strips
+   Float_t      fPitch;           // pitch
+
+private:
+   void         SetFunctions();
+   Int_t        GetAdcValue(Float_t charge);
+
+private:
+   static Float_t  fgkThres;
+   static Float_t  fgkFWTH;
+   static Float_t  fgDefSmearPos;
+   static Bool_t   fgSmearFlag;
+   static Float_t  fgkPairCreation;
+   static Float_t  fgkFanoFactor;
+   static Float_t  fgkNormFactor;
+};
+        
+
+#endif
+
