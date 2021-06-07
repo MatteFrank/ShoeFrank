2fde00dc4cb8432404ccc9f06a6486b3045beb01
Christian Finck <cfinck@iphc.cnrs.fr>
2021-06-07 16:14:59 +0200

    Add new executables Add new fragment  DEMSDEvent

---
+++ libs/src/TAGdaqApi/DEMSDEvent.hh
@@ -0,0 +1,28 @@
+#ifndef DEMSDEVENT_HH
+#define DEMSDEVENT_HH
+
+#include "DECardEvent.hh"
+#include <vector>
+
+class DEMSDEvent : public DECardEvent {
+
+  public:
+    virtual ~DEMSDEvent();
+  
+    virtual void readData(unsigned int **p);
+    virtual void printData() const;
+    virtual bool check() const;
+
+    virtual std::string classType() const {return "DECardEvent";};
+
+    u_int Xplane[640];
+    u_int Yplane[640];
+
+  void fillStrip();
+
+
+  
+  
+};
+
+#endif
