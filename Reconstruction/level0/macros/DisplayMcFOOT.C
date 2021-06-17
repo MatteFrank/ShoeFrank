4d6ec04010e23e3f5e12ec83c4a6f9e79b556d71
Christian Finck <cfinck@iphc.cnrs.fr>
2021-06-17 16:13:04 +0200

    Update macros

--- Reconstruction/level0/macros/DisplayMcFOOT.C
+++ Reconstruction/level0/macros/DisplayMcFOOT.C
@@ -9,8 +9,10 @@
 #include "TAGeventDisplayMC.hxx"
 #endif
 
+void DisplayMcFOOT(TString file = "16O_C_200_trig_shoereg.root", TString expName = "GSI2021_MC", Int_t runNumber = 200)
+//void DisplayMcFOOT(TString file = "12C_C_200new_trig_shoereg.root", TString expName = "12C_200new", Int_t runNumber = 1)
 //void DisplayMcFOOT(TString file = "12C_C_200shoe.root", TString expName = "12C_200", Int_t runNumber = 1)
-void DisplayMcFOOT(TString file = "12C_C_200.root", TString expName = "12C_200", Int_t runNumber = 1)
+//void DisplayMcFOOT(TString file = "12C_C_200.root", TString expName = "12C_200", Int_t runNumber = 1)
 {
    TAGbaseEventDisplay::SetVtxTrackingAlgo('F');
 //   TAGbaseEventDisplay::EnableItrTracking();
