48d523368c727da9bfc1f4d90eb6e416cbca9d5d
Christian Finck <cfinck@iphc.cnrs.fr>
2021-06-21 11:10:47 +0200

    Specified configuration file for MSD (still inherits from VT, tmp solution)

--- libs/src/TAMSDbase/TAMSDparConf.hxx
+++ libs/src/TAMSDbase/TAMSDparConf.hxx
@@ -2,7 +2,7 @@
 #define _TAMSDparConf_HXX
 /*!
   \file
-  \version $Id: TAMSDparConf.hxx,v 1.2 2003/06/22 19:33:36 mueller Exp $
+  \version $Id: TAMSDparConf.hxx
   \brief   Declaration of TAMSDparConf.
 */
 /*------------------------------------------+---------------------------------*/
@@ -16,13 +16,40 @@
 class TAMSDparConf : public TAVTbaseParConf {
    
 public:
-   
    TAMSDparConf();
    virtual ~TAMSDparConf();
    
+private:
+   Int_t      fSensorsN;         // Number of sensors
+   TString    fkDefaultConfName; // default detector configuration file
    
-   Bool_t     FromFile(const TString& name = "");
+   struct SensorParameter_t : public  TObject {
+      Int_t     SensorIdx;              // sensor index
+      Int_t     Type;                   // sensor type
+      Int_t     Status;                 // Status flag: Primary Reference = 1., Secondary Reference = 2. DeviceUunderTest = 3. Out = -1.
+      Int_t     MinNofStripsInCluster;  // minimum strips in a cluster
+      Int_t     MaxNofStripsInCluster;  // maximum strips in a cluster
+      TArrayC   DeadStripMap;           // dead strip map
+   };
    
+   struct AnalysisParameter_t : public  TObject {
+      Float_t    SigmaNoiseLevel;      // sigma noise level
+   };
+   
+   SensorParameter_t   fSensorParameter[6];
+   AnalysisParameter_t fAnalysisParameter;
+
+public:
+   Int_t         GetStatus(Int_t idx) const;
+   Int_t         GetStatusMinor(Int_t idx) const ;
+   Bool_t        FromFile(const TString& name = "");
+   
+   Int_t         GetSensorsN()          const  { return fSensorsN;                }
+   const Char_t* GetDefaultConfName()   const  { return fkDefaultConfName.Data(); }
+   
+   SensorParameter_t& GetSensorPar(Int_t idx)  { return fSensorParameter[idx];    }
+   AnalysisParameter_t& GetAnalysisPar()       { return fAnalysisParameter;       }
+
    
    ClassDef(TAMSDparConf,1)
 };
