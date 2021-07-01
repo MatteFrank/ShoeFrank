9b2f642cb19216a46ea8e6aed195d9433414d792
Christian Finck <cfinck@iphc.cnrs.fr>
2021-07-01 09:51:02 +0200

    Re-ordering class

--- libs/src/TAGbase/TAGcluster.hxx
+++ libs/src/TAGbase/TAGcluster.hxx
@@ -35,53 +35,51 @@ public:
    virtual           ~TAGcluster() {}
 
    //! Get validity
-   Bool_t                   IsValid()          const { return fIsValid;    }
+   Bool_t                   IsValid()          const { return fIsValid;       }
    //! Get type
-   Int_t                    GetDeviceType()    const { return fDeviceType; }
+   Int_t                    GetDeviceType()    const { return fDeviceType;    }
    //! Get major type
    Int_t                    GetDevMajorType()  const { return fDeviceType/10; }
    //! Get minor type
    Int_t                    GetDevMinorType()  const { return fDeviceType%10; }
    //! Get cluster index
-   Int_t                    GetClusterIdx()    const { return fClusterIdx; }
+   Int_t                    GetClusterIdx()    const { return fClusterIdx;    }
    //! Get sensor index
-   Int_t                    GetSensorIdx()     const { return fSensorIdx;  }
+   Int_t                    GetSensorIdx()     const { return fSensorIdx;     }
+   //! Get position in local frame
+   virtual const TVector3&  GetPosition()      const { return fPosition;      }
+   //! Get position error in local frame
+   virtual const TVector3&  GetPosError()      const { return fPosError;      }
+   //! Get position in global tracker frame
+   virtual const TVector3&  GetPositionG()     const { return fPositionG;     }
+   //! Get position in global tracker frame
+   virtual const TVector3&  GetPosErrorG()     const { return fPosErrorG;     }
+   //! Get MC info
+   Int_t                    GetMcTrackIdx(Int_t index) const   { return fMcTrackIdx[index];    }
+   Int_t                    GetMcTracksN()             const   { return fMcTrackIdx.GetSize(); }
+   
+   //! Set validy
+   void                     SetValid(Bool_t v = true)                     { fIsValid = v;             }
+   //! Set cluster type
+   void                     SetDeviceType(Int_t nb)                       { fDeviceType = nb;         }
+   //! Set cluster number
+   void                     SetClusterIdx(Int_t nb)                       { fClusterIdx = nb;         }
+   //! Set plane number
+   void                     SetSensorIdx(Int_t nb)                        { fSensorIdx = nb;          }
    //! Set position in local frame
    virtual void             SetPosition(TVector3& pos);
-   virtual void             SetPosition(Float_t u, Float_t v, Float_t z) { fPosition.SetXYZ(u,v,z); }
+   virtual void             SetPosition(Float_t u, Float_t v, Float_t z)  { fPosition.SetXYZ(u,v,z);  }
    //! Set position error in local frame
    virtual void             SetPosError(TVector3& pos);
-   virtual void             SetPosError(Float_t u, Float_t v, Float_t z) { fPosError.SetXYZ(u,v,z); }
+   virtual void             SetPosError(Float_t u, Float_t v, Float_t z)  { fPosError.SetXYZ(u,v,z);  }
    //! Set position in global tracker frame
    virtual void             SetPositionG(TVector3& pos);
    virtual void             SetPositionG(Float_t u, Float_t v, Float_t z) { fPositionG.SetXYZ(u,v,z); }
    //! Set position error in global tracker frame
    virtual void             SetPosErrorG(TVector3& pos);
    virtual void             SetPosErrorG(Float_t u, Float_t v, Float_t z) { fPosErrorG.SetXYZ(u,v,z); }
-
-   //! Set validy
-   void                     SetValid(Bool_t v = true){ fIsValid = v;       }
-   //! Set cluster type
-   void                     SetDeviceType(Int_t nb)  { fDeviceType = nb;   }
-   //! Set cluster number
-   void                     SetClusterIdx(Int_t nb)  { fClusterIdx = nb;   }
-   //! Set plane number
-   void                     SetSensorIdx(Int_t nb)   { fSensorIdx = nb;    }
-   //! Get position in local frame
-  virtual const TVector3&   GetPosition()      const { return fPosition;   }
-   //! Get position error in local frame
-  virtual const TVector3&   GetPosError()      const { return fPosError;   }
-   //! Get position in global tracker frame
-  virtual const TVector3&   GetPositionG()     const { return fPositionG ; }
-   //! Get position in global tracker frame
-  virtual const TVector3&   GetPosErrorG()     const { return fPosErrorG ; }
-   
    //! Add MC track Idx
-   void               AddMcTrackIdx(Int_t trackIdx);
-   
-   //! Get MC info
-   Int_t              GetMcTrackIdx(Int_t index)      const   { return fMcTrackIdx[index];    }
-   Int_t              GetMcTracksN()                  const   { return fMcTrackIdx.GetSize(); }
+   void                     AddMcTrackIdx(Int_t trackIdx);
    
     ClassDef(TAGcluster,2)
 };
