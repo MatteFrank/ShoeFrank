b778237132ac46ee2ed655b2f4ca0d154c392661
gtraini <giacomo.traini@roma.infn.it>
2021-07-16 00:49:21 +0200

    added trigger histos

--- libs/src/TACAbase/TAGactWDreader.hxx
+++ libs/src/TACAbase/TAGactWDreader.hxx
@@ -15,7 +15,7 @@
 #include "TACAntuRaw.hxx"
 #include "TAGbaseWDparTime.hxx"
 #include "TAGbaseWDparMap.hxx"
-
+#include <TH2F.h>
 
 class WDEvent;
 
@@ -65,7 +65,8 @@ public:
   TH1F *hTW[20][4];
   TH1F *hCalo[20][9];
   TH1F *hClk[20];
-
+  TH1F *hTrig;
+  TH2F *hTrigClk;
 
   int m_nev;
   
