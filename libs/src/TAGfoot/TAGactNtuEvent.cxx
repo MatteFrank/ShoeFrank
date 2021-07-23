7d224cc80b115bba46a887ea8b99e9ac43c3b272
Christian Finck <cfinck@iphc.cnrs.fr>
2021-07-23 15:16:47 +0200

    Put condition on trigger & info presence

--- libs/src/TAGfoot/TAGactNtuEvent.cxx
+++ libs/src/TAGfoot/TAGactNtuEvent.cxx
@@ -41,28 +41,27 @@ TAGactNtuEvent::~TAGactNtuEvent()
 //! Action.
 Bool_t TAGactNtuEvent::Action()
 {
-  TAGdaqEvent* datDaq = (TAGdaqEvent*)  fpDatDaq->Object();
-  TAGntuEvent* pNtuEvt = (TAGntuEvent*)  fpNtuEvt->Object();
+  TAGdaqEvent* datDaq  = (TAGdaqEvent*) fpDatDaq->Object();
+  TAGntuEvent* pNtuEvt = (TAGntuEvent*) fpNtuEvt->Object();
   
-  InfoEvent*  infoEvent = datDaq->GetInfoEvent();
-  if (!infoEvent) return true;
-  TrgEvent*   trgEvent  = datDaq->GetTrgEvent();
-  if (!trgEvent) return true;
-
-  if (trgEvent->eventNumber != infoEvent->eventNumber)
-    Warning("Action()", "Event number different in trigger and info event");
+  InfoEvent* infoEvent = datDaq->GetInfoEvent();
+  TrgEvent*  trgEvent  = datDaq->GetTrgEvent();
+   
+  if (infoEvent && trgEvent) {
+     if (trgEvent->eventNumber != infoEvent->eventNumber)
+        Warning("Action()", "Event number different in trigger and info event");
   
-  pNtuEvt->SetTimeSec(trgEvent->time_sec);
-  pNtuEvt->SetTimeUSec(trgEvent->time_usec);
-  pNtuEvt->SetEventNumber(trgEvent->eventNumber);
-  pNtuEvt->SetLiveTime(trgEvent->liveTime);
-  pNtuEvt->SetTimeSinceLastTrigger(trgEvent->timeSinceLastTrigger);
-  pNtuEvt->SetClockCounter(trgEvent->clockCounter);
-  pNtuEvt->SetTriggerCounter(trgEvent->triggerCounter);
-  pNtuEvt->SetBCOofTrigger(trgEvent->BCOofTrigger);
-  pNtuEvt->SetSpillNrAndTrgFineDelay(trgEvent->spillNrAndTrgFineDelay);
-  pNtuEvt->SetPMTsAndBusy(trgEvent->PMTsAndBusy);
-
+     pNtuEvt->SetTimeSec(trgEvent->time_sec);
+     pNtuEvt->SetTimeUSec(trgEvent->time_usec);
+     pNtuEvt->SetEventNumber(trgEvent->eventNumber);
+     pNtuEvt->SetLiveTime(trgEvent->liveTime);
+     pNtuEvt->SetTimeSinceLastTrigger(trgEvent->timeSinceLastTrigger);
+     pNtuEvt->SetClockCounter(trgEvent->clockCounter);
+     pNtuEvt->SetTriggerCounter(trgEvent->triggerCounter);
+     pNtuEvt->SetBCOofTrigger(trgEvent->BCOofTrigger);
+     pNtuEvt->SetSpillNrAndTrgFineDelay(trgEvent->spillNrAndTrgFineDelay);
+     pNtuEvt->SetPMTsAndBusy(trgEvent->PMTsAndBusy);
+  }
   SetBit(kValid);
   fpNtuEvt->SetBit(kValid);
   
