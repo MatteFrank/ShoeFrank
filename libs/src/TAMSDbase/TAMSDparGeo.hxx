92502ee893eedb70c01ee53f499395237fa579aa
Christian Finck <cfinck@iphc.cnrs.fr>
2021-07-26 10:41:30 +0200

    Correct return value for station getter

--- libs/src/TAMSDbase/TAMSDparGeo.hxx
+++ libs/src/TAMSDbase/TAMSDparGeo.hxx
@@ -46,11 +46,11 @@ public:
   //! Get number of strip
   Int_t       GetStripsN()             const { return fStripsN; }
    
-   //! Get pitch
+  //! Get pitch
   Float_t     GetPitch()               const { return fPitch;   }
    
-  //! Get pitch
-  Float_t     GetStationsN()           const { return (fSensorsN % 2 == 0) ? fSensorsN/2 : fSensorsN/2 + 1;  }
+  //! Get stationsN
+  Int_t       GetStationsN()           const { return (fSensorsN % 2 == 0) ? fSensorsN/2 : fSensorsN/2 + 1;  }
    
   //crossing regions
   Int_t       GetRegStrip(Int_t n);   //n=sensor (0-5)
