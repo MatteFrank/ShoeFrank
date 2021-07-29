733b9ebcfd9d5f32dca50da06b2f8e7cce0fc368
Christian Finck <cfinck@iphc.cnrs.fr>
2021-07-29 17:53:20 +0200

    First attempt for resynchronising, under progress

--- libs/src/TAVTbase/TAVTactNtuHit.cxx
+++ libs/src/TAVTbase/TAVTactNtuHit.cxx
@@ -29,7 +29,8 @@ ClassImp(TAVTactNtuHit);
 TAVTactNtuHit::TAVTactNtuHit(const char* name, TAGdataDsc* pNtuRaw, TAGdataDsc* pDatDaq, TAGparaDsc* pGeoMap, TAGparaDsc* pConfig, TAGparaDsc* pParMap)
 : TAVTactBaseNtuHit(name, pNtuRaw, pGeoMap, pConfig, pParMap),
   fpDatDaq(pDatDaq),
-  fFirstBcoTrig(0)
+  fFirstBcoTrig(0),
+  fQueueEvtsN(0)
 {
    AddDataIn(pDatDaq, "TAGdaqEvent");
 }
@@ -50,22 +51,39 @@ Bool_t TAVTactNtuHit::Action()
    UInt_t bcoTrig = 0;
    static Bool_t first = false;
    
+   const DECardEvent* evt = 0x0;
+   const DECardEvent* evt0 = 0x0;
+   static const DECardEvent* evtp = 0x0;
+
+   static UInt_t prefBco = 0;
    for (Int_t i = 0; i < nFragments; ++i) {
       
        TString type = datDaq->GetClassType(i);
        if (type.Contains("DECardEvent")) {
-          const DECardEvent* evt = static_cast<const DECardEvent*> (datDaq->GetFragment(i));
+          evt0  = static_cast<const DECardEvent*> (datDaq->GetFragment(i));
+          
+          if (fQueueEvtsN == 0)
+             evt = evt0;
+          else {
+             evt = evtp;//fQueueEvt.front();
+             //fQueueEvt.push(evt0);
+          }
           bcoTrig    = evt->BCOofTrigger;
           fData      = evt->values;
           fEventSize = evt->evtSize;
           fDataLink  = evt->channelID - (dataVTX | 0x30);
           if (fEventSize == 0) continue;
           DecodeEvent();
+          
+  //        if (fQueueEvtsN > 0)
+//              fQueueEvt.pop();
        }
    }
    
    SetBit(kValid);
    fpNtuRaw->SetBit(kValid);
+   
+   if (!evt) return true;
    
    TrgEvent* trig = datDaq->GetTrgEvent();
    if (!trig) return true;
@@ -75,10 +93,23 @@ Bool_t TAVTactNtuHit::Action()
       first = true;
    }
    
-   Int_t diff = bcoTrig-trig->BCOofTrigger-fFirstBcoTrig;
+   Int_t diff    = bcoTrig - trig->BCOofTrigger - fFirstBcoTrig;
+   Int_t prediff = prefBco - trig->BCOofTrigger - fFirstBcoTrig;
    fpHisBCOofTrigger->Fill(diff);
-   if (TMath::Abs(float(diff)) > 3)
-      Warning("Action()", "BCOofTrigger difference higher than 3 (%d)", diff);
+   
+   if (TMath::Abs(float(diff)) > 3) {
+      Warning("Action()", "BCOofTrigger difference higher than 3 (%d) for %d time, resynchronizing", diff, fQueueEvtsN+1);
+      
+ //     fQueueEvt.push(evtp);
+      fQueueEvtsN++;
+   }
+   if (fQueueEvtsN > 0) {
+      if (evtp)
+         delete evtp;
+      evtp = new DECardEvent(*evt0);
+   }
+
+   prefBco = bcoTrig;
    
    return kTRUE;
 }
