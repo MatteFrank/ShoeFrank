5138ebc9ea03406f81235c192f10975717969d2b
Christian Finck <cfinck@iphc.cnrs.fr>
2021-10-08 14:57:01 +0200

    Put hardware wise the required clusters and search distance (to be put in config file)

--- libs/src/TAMSDbase/TAMSDactBaseNtuTrack.cxx
+++ libs/src/TAMSDbase/TAMSDactBaseNtuTrack.cxx
@@ -42,6 +42,8 @@ TAMSDactBaseNtuTrack::TAMSDactBaseNtuTrack(const char* name,
 										 TAGparaDsc* pGeoMap)
 : TAVTactBaseTrack(name, pNtuClus, pNtuTrack, pConfig, pGeoMap)
 {
+   fRequiredClusters = 3;
+   fSearchClusDistance = 0.1;
 }
 
 //------------------------------------------+-----------------------------------
@@ -156,7 +158,7 @@ Bool_t TAMSDactBaseNtuTrack::FindStraightTracks()
 	  // and try to make a track out of each one
 	  for( Int_t iLastClus = 0; iLastClus < nClusters; ++iLastClus) { // loop on cluster of last plane, 
 		 
-		 if( pNtuTrack->GetTracksN() >= pConfig->GetAnalysisPar().TracksMaximum ) break; // if max track number reach, stop
+		 //if( pNtuTrack->GetTracksN() >= pConfig->GetAnalysisPar().TracksMaximum ) break; // if max track number reach, stop
 		 
 		 TAMSDpoint* cluster = (TAMSDpoint*)list->At( iLastClus );
 		 if (cluster->Found()) continue;
