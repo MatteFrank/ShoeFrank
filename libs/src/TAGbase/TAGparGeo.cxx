7af9a11cd3436910d8467e33670a03377d675b21
lopeztor <ernesto.lopez@to.infn.it>
2022-06-27 10:19:18 +0200

    Cosmetic, identation

--- libs/src/TAGbase/TAGparGeo.cxx
+++ libs/src/TAGbase/TAGparGeo.cxx
@@ -286,7 +286,7 @@ TGeoVolume* TAGparGeo::AddCubicTarget(const char* targetName)
 }
 
 //_____________________________________________________________________________
-//! Build cylindric target for Root geometry
+//! Build cylindrical target for Root geometry
 //!
 //! \param[in] targetName target volume name
 TGeoVolume* TAGparGeo::AddCylindricTarget(const char* targetName)
@@ -397,93 +397,93 @@ void TAGparGeo::Print(Option_t* option) const
 //! Print Fluka standard bodies
 string TAGparGeo::PrintStandardBodies( )
 {
-  stringstream ss;
+   stringstream ss;
 
-  ss << "* ***Black Body" << endl;
-  ss << "RPP blk        -1000. 1000. -1000. 1000. -1000. 1000." << endl;
-  ss << "* ***Air" << endl;
-  ss << "RPP air        -900.0 900.0 -900.0 900.0 -900.0 900.0" << endl;
+   ss << "* ***Black Body" << endl;
+   ss << "RPP blk        -1000. 1000. -1000. 1000. -1000. 1000." << endl;
+   ss << "* ***Air" << endl;
+   ss << "RPP air        -900.0 900.0 -900.0 900.0 -900.0 900.0" << endl;
 
-  double zplane;
+   double zplane;
 
-  TAGgeoTrafo* fpFootGeo = (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
+   TAGgeoTrafo* fpFootGeo = (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
 
-  TVector3  fCenterTW = fpFootGeo->GetTWCenter();
-  TVector3  fCenterMSD = fpFootGeo->GetMSDCenter();
-  zplane =  fCenterMSD.Z() + ( fCenterTW.Z()-fCenterMSD.Z() ) /2.;
-  //needed to subdivide air in two because when the calo is present too many bodies
-  //are subtracted to air and fluka complains
-  if(TAGrecoManager::GetPar()->IncludeCA())
-    ss << "XYP airpla     " << zplane << endl;
+   TVector3  fCenterTW = fpFootGeo->GetTWCenter();
+   TVector3  fCenterMSD = fpFootGeo->GetMSDCenter();
+   zplane =  fCenterMSD.Z() + ( fCenterTW.Z()-fCenterMSD.Z() ) /2.;
+   //needed to subdivide air in two because when the calo is present too many bodies
+   //are subtracted to air and fluka complains
+   if(TAGrecoManager::GetPar()->IncludeCA())
+      ss << "XYP airpla     " << zplane << endl;
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
 //! Print Fluka rotations
 string TAGparGeo::PrintTargRotations()
 {
-  stringstream ss;
+   stringstream ss;
 
-  if(TAGrecoManager::GetPar()->IncludeTG()){
+   if(TAGrecoManager::GetPar()->IncludeTG()){
 
-    TAGgeoTrafo* fpFootGeo = (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
+      TAGgeoTrafo* fpFootGeo = (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
 
-    TVector3 fCenter = fpFootGeo->GetTGCenter();
-    TVector3  fAngle = fpFootGeo->GetTGAngles();
+      TVector3 fCenter = fpFootGeo->GetTGCenter();
+      TVector3  fAngle = fpFootGeo->GetTGAngles();
 
-    if(fAngle.X()!=0 || fAngle.Y()!=0 || fAngle.Z()!=0){
+      if(fAngle.X()!=0 || fAngle.Y()!=0 || fAngle.Z()!=0){
 
-      ss << PrintCard("ROT-DEFI", "300.", "", "",
-		      Form("%f",-fCenter.X()), Form("%f",-fCenter.Y()),
-		      Form("%f",-fCenter.Z()), "tg") << endl;
-      if(fAngle.X()!=0)
-	ss << PrintCard("ROT-DEFI", "100.", "", Form("%f",fAngle.X()),"", "", "", "tg") << endl;
-      if(fAngle.Y()!=0)
-	ss << PrintCard("ROT-DEFI", "200.", "", Form("%f",fAngle.Y()),"", "", "", "tg") << endl;
-      if(fAngle.Z()!=0)
-	ss << PrintCard("ROT-DEFI", "300.", "", Form("%f",fAngle.Z()),"", "", "", "tg") << endl;
-      ss << PrintCard("ROT-DEFI", "300.", "", "",
-		      Form("%f",fCenter.X()), Form("%f",fCenter.Y()),
-		      Form("%f",fCenter.Z()), "tg") << endl;
+         ss << PrintCard("ROT-DEFI", "300.", "", "",
+               Form("%f",-fCenter.X()), Form("%f",-fCenter.Y()),
+               Form("%f",-fCenter.Z()), "tg") << endl;
+         if(fAngle.X()!=0)
+      ss << PrintCard("ROT-DEFI", "100.", "", Form("%f",fAngle.X()),"", "", "", "tg") << endl;
+         if(fAngle.Y()!=0)
+      ss << PrintCard("ROT-DEFI", "200.", "", Form("%f",fAngle.Y()),"", "", "", "tg") << endl;
+         if(fAngle.Z()!=0)
+      ss << PrintCard("ROT-DEFI", "300.", "", Form("%f",fAngle.Z()),"", "", "", "tg") << endl;
+         ss << PrintCard("ROT-DEFI", "300.", "", "",
+               Form("%f",fCenter.X()), Form("%f",fCenter.Y()),
+               Form("%f",fCenter.Z()), "tg") << endl;
 
-    }
-  }
+      }
+   }
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
 //! Print Fluka target body
 string TAGparGeo::PrintTargBody( )
 {
-  stringstream ss;
+   stringstream ss;
 
-  if(TAGrecoManager::GetPar()->IncludeTG()){
-    ss << "* ***Target" << endl;
+   if(TAGrecoManager::GetPar()->IncludeTG()){
+      ss << "* ***Target" << endl;
 
-    double zero = 0.;
+      double zero = 0.;
 
-    TAGgeoTrafo* fpFootGeo = (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
+      TAGgeoTrafo* fpFootGeo = (TAGgeoTrafo*)gTAGroot->FindAction(TAGgeoTrafo::GetDefaultActName().Data());
 
-    TVector3  fCenter = fpFootGeo->GetTGCenter();
-    TVector3  fAngle = fpFootGeo->GetTGAngles();
+      TVector3  fCenter = fpFootGeo->GetTGCenter();
+      TVector3  fAngle = fpFootGeo->GetTGAngles();
 
-    TVector3 tgSize =  GetTargetPar().Size;
+      TVector3 tgSize =  GetTargetPar().Size;
 
-    if(fAngle.Mag()!=0)
-      ss << "$start_transform tg" << endl;
+      if(fAngle.Mag()!=0)
+         ss << "$start_transform tg" << endl;
 
-    ss << setiosflags(ios::fixed) << setprecision(6);
-    ss << "RPP tgt     "  << fCenter[0]-tgSize.X()/2. << " " << fCenter[0]+tgSize.X()/2 << " " <<
-      fCenter[1]-tgSize.Y()/2. << " " << fCenter[1]+tgSize.Y()/2. << " " <<
-      fCenter[2]-tgSize.Z()/2. << " " << fCenter[2]+tgSize.Z()/2. << " " <<  endl;
+      ss << setiosflags(ios::fixed) << setprecision(6);
+      ss << "RPP tgt     "  << fCenter[0]-tgSize.X()/2. << " " << fCenter[0]+tgSize.X()/2 << " " <<
+         fCenter[1]-tgSize.Y()/2. << " " << fCenter[1]+tgSize.Y()/2. << " " <<
+         fCenter[2]-tgSize.Z()/2. << " " << fCenter[2]+tgSize.Z()/2. << " " <<  endl;
 
-    if(fAngle.Mag()!=0)
-      ss << "$end_transform" << endl;
+      if(fAngle.Mag()!=0)
+         ss << "$end_transform" << endl;
 
-  }
-  return ss.str();
+   }
+   return ss.str();
 }
 
 
@@ -491,16 +491,16 @@ string TAGparGeo::PrintTargBody( )
 //! for blackbody and air
 string TAGparGeo::PrintStandardRegions1() {
 
-  stringstream ss;
+   stringstream ss;
 
-  ss <<"BLACK        5 blk -air\n";
-  ss <<"* ***Air\n";
-  if(TAGrecoManager::GetPar()->IncludeCA())
-    ss <<"AIR1          5 air +airpla";
-  else
-    ss <<"AIR1          5 air";
+   ss <<"BLACK        5 blk -air\n";
+   ss <<"* ***Air\n";
+   if(TAGrecoManager::GetPar()->IncludeCA())
+      ss <<"AIR1         5 air +airpla";
+   else
+      ss <<"AIR1          5 air";
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
@@ -508,39 +508,39 @@ string TAGparGeo::PrintStandardRegions1() {
 //!are subtracted to air and fluka complains so it's necessary to subdivide air in 2 parts
 string TAGparGeo::PrintStandardRegions2()
 {
-  stringstream ss;
+   stringstream ss;
 
-  if(TAGrecoManager::GetPar()->IncludeCA())
-    ss <<"AIR2          5 air -airpla";
+   if(TAGrecoManager::GetPar()->IncludeCA())
+      ss << endl << "AIR2         5 air -airpla";
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
 //! Print Fluka target region
 string TAGparGeo::PrintTargRegion()
 {
-  stringstream ss;
+   stringstream ss;
 
-  if(TAGrecoManager::GetPar()->IncludeTG()){
-    ss << "* ***Target" << endl;
-    ss << "TARGET      5 +tgt" << endl;
-  }
+   if(TAGrecoManager::GetPar()->IncludeTG()){
+      ss << "* ***Target" << endl;
+      ss << "TARGET      5 +tgt" << endl;
+   }
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
 //! Print Fluka subtracted target body from air
 string TAGparGeo::PrintSubtractTargBodyFromAir()
 {
-  stringstream ss;
+   stringstream ss;
 
-  if(TAGrecoManager::GetPar()->IncludeTG()){
-    ss << "-tgt " << endl;
-  }
+   if(TAGrecoManager::GetPar()->IncludeTG()){
+      ss << "-tgt " << endl;
+   }
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
@@ -549,111 +549,111 @@ string TAGparGeo::PrintSubtractTargBodyFromAir()
 //! \param[in] Material Root material
 string TAGparGeo::PrintTargAssignMaterial(TAGmaterials* Material)
 {
-  stringstream outstr;
+   stringstream outstr;
 
-  if(TAGrecoManager::GetPar()->IncludeTG()){
+   if(TAGrecoManager::GetPar()->IncludeTG()){
 
-    TString flkmat;
+      TString flkmat;
 
-    if (Material == NULL){
-      TAGmaterials::Instance()->PrintMaterialFluka();
-      flkmat = TAGmaterials::Instance()->GetFlukaMatName(GetTargetPar().Material);
-    }
-    else
-      flkmat = Material->GetFlukaMatName(GetTargetPar().Material);
+      if (Material == NULL){
+         TAGmaterials::Instance()->PrintMaterialFluka();
+         flkmat = TAGmaterials::Instance()->GetFlukaMatName(GetTargetPar().Material);
+      }
+      else
+         flkmat = Material->GetFlukaMatName(GetTargetPar().Material);
 
-    bool magnetic = false;
-    if(TAGrecoManager::GetPar()->IncludeDI())
-      magnetic = true;
+      bool magnetic = false;
+      if(TAGrecoManager::GetPar()->IncludeDI())
+         magnetic = true;
 
-    outstr << PrintCard("ASSIGNMA", flkmat, "TARGET", "", "", Form("%d",magnetic), "", "") << endl;
+      outstr << PrintCard("ASSIGNMA", flkmat, "TARGET", "", "", Form("%d",magnetic), "", "") << endl;
 
-  }
+   }
 
-  return outstr.str();
+   return outstr.str();
 }
 
 //_____________________________________________________________________________
 //! Print standard assigned material in Fluka for a given material
 string TAGparGeo::PrintStandardAssignMaterial()
 {
-  stringstream ss;
+   stringstream ss;
 
-  int magnetic = 0;
-  if(TAGrecoManager::GetPar()->IncludeDI())
-    magnetic = 1;
+   int magnetic = 0;
+   if(TAGrecoManager::GetPar()->IncludeDI())
+      magnetic = 1;
 
-  ss << PrintCard("ASSIGNMA","BLCKHOLE", "BLACK","","","","","") << endl;
-  ss << PrintCard("ASSIGNMA","AIR","AIR1","","",TString::Format("%d",magnetic),"","") << endl;
+   ss << PrintCard("ASSIGNMA","BLCKHOLE", "BLACK","","","","","") << endl;
+   ss << PrintCard("ASSIGNMA","AIR","AIR1","","",TString::Format("%d",magnetic),"","") << endl;
 
-  if(TAGrecoManager::GetPar()->IncludeCA())
-    ss << PrintCard("ASSIGNMA","AIR","AIR2","","",TString::Format("%d",magnetic),"","") << endl;
+   if(TAGrecoManager::GetPar()->IncludeCA())
+      ss << PrintCard("ASSIGNMA","AIR","AIR2","","",TString::Format("%d",magnetic),"","") << endl;
 
-  return ss.str();
+   return ss.str();
 }
 
 //_____________________________________________________________________________
 //! Print Fluka beam
 string TAGparGeo::PrintBeam()
 {
-  stringstream str;
+   stringstream str;
 
-  string part_type;
-  if (GetBeamPar().AtomicNumber>2)
-    part_type = "HEAVYION";
-  else if (GetBeamPar().AtomicNumber==1 && GetBeamPar().AtomicMass==1)
-    part_type = "PROTON";
-  else if (GetBeamPar().AtomicNumber==2 && GetBeamPar().AtomicMass==4)
-    part_type = "4-HELIUM";
-  else{
-    cout << "**** ATTENTION: unknown beam!!!! ****"<< endl;
-    exit(0);
-  }
+   string part_type;
+   if (GetBeamPar().AtomicNumber>2)
+      part_type = "HEAVYION";
+   else if (GetBeamPar().AtomicNumber==1 && GetBeamPar().AtomicMass==1)
+      part_type = "PROTON";
+   else if (GetBeamPar().AtomicNumber==2 && GetBeamPar().AtomicMass==4)
+      part_type = "4-HELIUM";
+   else{
+      cout << "**** ATTENTION: unknown beam!!!! ****"<< endl;
+      exit(0);
+   }
 
-  str << PrintCard("BEAM",TString::Format("%f",-(GetBeamPar().Energy)), "",
-		   TString::Format("%f",GetBeamPar().AngDiv),
-		   TString::Format("%f",-GetBeamPar().Size),
-		   TString::Format("%f",-GetBeamPar().Size),
-		   "1.0",part_type) << endl;
-  if(part_type == "HEAVYION")
-    str << PrintCard("HI-PROPE",TString::Format("%d",GetBeamPar().AtomicNumber),
-		     TString::Format("%.0f",GetBeamPar().AtomicMass),"","","","","") << endl;
-  str << PrintCard("BEAMPOS",TString::Format("%.3f",GetBeamPar().Position.X()),
-		   TString::Format("%.3f",GetBeamPar().Position.Y()),
-		   TString::Format("%.3f",GetBeamPar().Position.Z()),"","","","") << endl;
+   str << PrintCard("BEAM",TString::Format("%f",-(GetBeamPar().Energy)), "",
+            TString::Format("%f",GetBeamPar().AngDiv),
+            TString::Format("%f",-GetBeamPar().Size),
+            TString::Format("%f",-GetBeamPar().Size),
+            "1.0",part_type) << endl;
+   if(part_type == "HEAVYION")
+      str << PrintCard("HI-PROPE",TString::Format("%d",GetBeamPar().AtomicNumber),
+            TString::Format("%.0f",GetBeamPar().AtomicMass),"","","","","") << endl;
+   str << PrintCard("BEAMPOS",TString::Format("%.3f",GetBeamPar().Position.X()),
+            TString::Format("%.3f",GetBeamPar().Position.Y()),
+            TString::Format("%.3f",GetBeamPar().Position.Z()),"","","","") << endl;
 
-  return str.str();
+   return str.str();
 }
 
 //_____________________________________________________________________________
 //! Print Fluka physics
 string TAGparGeo::PrintPhysics()
 {
-  stringstream str;
+   stringstream str;
 
-  if ( TAGrecoManager::GetPar()->verFLUKA() ) {
-    str << PrintCard("PHYSICS","1.","","","","","","COALESCE") << endl;
-    str << PrintCard("PHYSICS","3.","","","","","","EVAPORAT") << endl;
-    str << PrintCard("PHYSICS","1.","","","","","","ISOMERS") << endl;
-    str << PrintCard("PHYSICS","1.","0.005","0.15","2.","2.","2.","IONSPLIT") << endl;
-    str << PrintCard("RADDECAY","2.","","1.","","","","") << endl;
-  }  else {
-    str << PrintCard("PHYSICS","212001.","1.","1.","","","","COALESCE") << endl;
-    str << PrintCard("PHYSICS","3.","","","","","","EVAPORAT") << endl;
-    str << PrintCard("PHYSICS","1.","","","","","","ISOMERS") << endl;
-    str << PrintCard("PHYSICS","1.","","","","","","INFLDCAY") << endl;
-    str << PrintCard("PHYSICS","1.","","","","","","QUASI-EL") << endl;
-    str << PrintCard("PHYSICS","1.","0.005","0.15","2.","2.","2.","IONSPLIT") << endl;
-    str << PrintCard("RADDECAY","1.","2.","","1.","","","") << endl;
-  }
-  str << PrintCard("EMFCUT","-1.","1.","","BLACK","@LASTREG","1.0","") << endl;
-  str << PrintCard("EMFCUT","-1.","1.","1.","BLCKHOLE","@LASTMAT","1.0","PROD-CUT") << endl;
-  str << PrintCard("DELTARAY","1.","","","BLCKHOLE","@LASTMAT","1.0","") << endl;
-  str << PrintCard("PAIRBREM","-3.","","","BLCKHOLE","@LASTMAT","","") << endl;
+   if ( TAGrecoManager::GetPar()->verFLUKA() ) {
+      str << PrintCard("PHYSICS","1.","","","","","","COALESCE") << endl;
+      str << PrintCard("PHYSICS","3.","","","","","","EVAPORAT") << endl;
+      str << PrintCard("PHYSICS","1.","","","","","","ISOMERS") << endl;
+      str << PrintCard("PHYSICS","1.","0.005","0.15","2.","2.","2.","IONSPLIT") << endl;
+      str << PrintCard("RADDECAY","2.","","1.","","","","") << endl;
+   }  else {
+      str << PrintCard("PHYSICS","212001.","1.","1.","","","","COALESCE") << endl;
+      str << PrintCard("PHYSICS","3.","","","","","","EVAPORAT") << endl;
+      str << PrintCard("PHYSICS","1.","","","","","","ISOMERS") << endl;
+      str << PrintCard("PHYSICS","1.","","","","","","INFLDCAY") << endl;
+      str << PrintCard("PHYSICS","1.","","","","","","QUASI-EL") << endl;
+      str << PrintCard("PHYSICS","1.","0.005","0.15","2.","2.","2.","IONSPLIT") << endl;
+      str << PrintCard("RADDECAY","1.","2.","","1.","","","") << endl;
+   }
+   str << PrintCard("EMFCUT","-1.","1.","","BLACK","@LASTREG","1.0","") << endl;
+   str << PrintCard("EMFCUT","-1.","1.","1.","BLCKHOLE","@LASTMAT","1.0","PROD-CUT") << endl;
+   str << PrintCard("DELTARAY","1.","","","BLCKHOLE","@LASTMAT","1.0","") << endl;
+   str << PrintCard("PAIRBREM","-3.","","","BLCKHOLE","@LASTMAT","","") << endl;
 
-  if(TAGrecoManager::GetPar()->IncludeDI()){
-    str << PrintCard("MGNFIELD","0.1","0.00001","","0.","0.","0.","") << endl;
-  }
+   if(TAGrecoManager::GetPar()->IncludeDI()){
+      str << PrintCard("MGNFIELD","0.1","0.00001","","0.","0.","0.","") << endl;
+   }
 
-  return str.str();
+   return str.str();
 }
