1409f60c24fd21556d7c3131ec791fab9f968eb6
lopeztor <ernesto.lopez@to.infn.it>
2022-07-12 10:09:44 +0200

    New clustering algorithm

--- libs/src/TACAbase/TACAactNtuCluster.cxx
+++ libs/src/TACAbase/TACAactNtuCluster.cxx
@@ -34,18 +34,18 @@ TACAactNtuCluster::TACAactNtuCluster(const char* name, TAGdataDsc* pNtuRaw, TAGd
    fpConfig(pConfig),
    fpGeoMap(pGeoMap),
    fpNtuTwPoint(pTwPoint),
-   fCurrentPosition(0., 0., 0.),
-   fCurrentPosError(0., 0., 0.),
+//   fCurrentPosition(0., 0., 0.),
+//   fCurrentPosError(0., 0., 0.),
    fClustersN(0)
 {
    AddDataIn(pNtuRaw,   "TACAntuHit");
    AddDataOut(pNtuClus, "TACAntuCluster");
-   AddPara(pGeoMap, "TACAparGeo");
-  // AddPara(pConfig, "TACAparConf");
+   AddPara(pGeoMap,     "TACAparGeo");
+   //AddPara(pConfig, "TACAparConf");
 
    fDimY = 18;
    fDimX = 18;
-    SetupMaps(fDimY*fDimX);
+   SetupMaps(fDimY*fDimX);
 }
 
 //------------------------------------------+-----------------------------------
@@ -95,9 +95,10 @@ void TACAactNtuCluster::CreateHistogram()
 //
 Bool_t TACAactNtuCluster::Action()
 {
-   TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
 
-   if (pNtuHit->GetHitsN() == 0) {
+   fpNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
+
+   if (fpNtuHit->GetHitsN() == 0) {
       fpNtuClus->SetBit(kValid);
       return true;
    }
@@ -120,11 +121,11 @@ void TACAactNtuCluster::FillMaps()
    ClearMaps();
 
    TACAparGeo* pGeoMap  = (TACAparGeo*) fpGeoMap->Object();
-   TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
+   //TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
 
    // fill maps for cluster
-   for (Int_t i = 0; i < pNtuHit->GetHitsN(); i++) { // loop over hit crystals
-      TACAhit* hit = pNtuHit->GetHit(i);
+   for (Int_t i = 0; i < fpNtuHit->GetHitsN(); i++) { // loop over hit crystals
+      TACAhit* hit = fpNtuHit->GetHit(i);
       if(!hit->IsValid()) continue;
       Int_t id   = hit->GetCrystalId();
       Int_t line = pGeoMap->GetCrystalLine(id);
@@ -137,17 +138,101 @@ void TACAactNtuCluster::FillMaps()
 }
 
 //______________________________________________________________________________
-//
+//! Shape cluster by region growing algorithm
+//! Condition to grow: hit charge should be less than seed charge
+//!
+//! \param[in] numClus cluster number
+//! \param[in] IndX index in X
+//! \param[in] IndY index in Y
+//! \param[in] seedCharge charge of previous seed
+Bool_t TACAactNtuCluster::ShapeCluster(Int_t numClus, Int_t IndX, Int_t IndY, double seedCharge)
+{
+   Int_t idx = IndY*fDimX+IndX;
+   if ( fPixelMap.count(idx) == 0 ) return false; // empty place
+   if ( fFlagMap[idx] != -1 ) return false; // already flagged
+
+   int index = fIndexMap[idx];
+   TAGobject* pixel = GetHitObject(index);
+   TACAhit* hit = (TACAhit*)pixel;
+   double charge = hit->GetCharge();
+
+   //if( charge < fChargeThreshold ) return false;
+
+   //if( charge > fFactor * seedCharge ) return false;
+   if( charge > seedCharge ) return false;
+
+   fFlagMap[idx] = numClus;
+   pixel->SetFound(true);
+
+   
+   for(Int_t i = -1; i <= 1 ; ++i)
+      if (CheckLine(IndX+i)) {
+         ShapeCluster(numClus, IndX+i, IndY, charge);
+      }
+   
+   for(Int_t j = -1; j <= 1 ; ++j)
+      if (CheckCol(IndY+j)) {
+         ShapeCluster(numClus, IndX, IndY+j, charge);
+      }
+   
+   return true;
+}
+
+//______________________________________________________________________________
+//! Found all clusters. Starting seed from local maximum 
 void TACAactNtuCluster::SearchCluster()
 {
    fClustersN = 0;
 
    TACAparGeo* pGeoMap  = (TACAparGeo*) fpGeoMap->Object();
-   TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
+
+   int nCry = pGeoMap->GetCrystalsN();
 
    // Search for cluster
-   for (Int_t i = 0; i < pNtuHit->GetHitsN(); ++i) { // loop over hit crystals
-      TACAhit* hit = pNtuHit->GetHit(i);
+   int loopCounter = 0;
+   while (true) {
+      double maxCharge = 0;
+      int indexSeed = -1;
+      for (Int_t i = 0; i < fpNtuHit->GetHitsN(); ++i) { // loop over hit crystals
+         TACAhit* hit = fpNtuHit->GetHit(i);
+         if (!hit->IsValid()) continue;
+         if (hit->Found()) continue;
+         double charge = hit->GetCharge();
+         if (charge > maxCharge) {
+            maxCharge = charge;
+            indexSeed = i;
+         }
+      }
+
+      if (indexSeed == -1) break; // maximum not found, not more clusters
+
+      TACAhit* hit = fpNtuHit->GetHit(indexSeed);
+
+      Int_t id   = hit->GetCrystalId();
+      Int_t line = pGeoMap->GetCrystalLine(id);
+      Int_t col  = pGeoMap->GetCrystalCol(id);
+      if (!CheckLine(line)) continue;
+      if (!CheckCol(col)) continue;
+
+      // Find cluster by region growing
+      if ( ShapeCluster(fClustersN, col, line, hit->GetCharge() + 0.1) ) // +0.1 to meet condition on the seed
+         fClustersN++;   
+      else 
+         break;   
+
+      if ( ++loopCounter > nCry ) { // this should not happen (we can not have more clusters than crystals)
+         Error("TACAactNtuCluster::SearchCluster", 
+               "We got infinite loop!! Getting out.. num. cluster: %d, Event: %d",
+               fClustersN, gTAGroot->CurrentEventId().EventNumber());
+         break;
+      }
+   }
+
+   cout << "CA - Found : " << fClustersN << " clusters on event: " << gTAGroot->CurrentEventId().EventNumber() << endl;
+
+   /*
+   for (Int_t i = 0; i < fpNtuHit->GetHitsN(); ++i) { // loop over hit crystals
+      TACAhit* hit = fpNtuHit->GetHit(i);
 
       if (hit->Found()) continue;
       if (!hit->IsValid()) continue;
@@ -163,19 +248,19 @@ void TACAactNtuCluster::SearchCluster()
          fClustersN++;
 
    }
+   */
 }
 
 //______________________________________________________________________________
-// Get object in list
+//! Get Hit object in list
+//!
+//! \param[in] idx index on Hit list
 TAGobject*  TACAactNtuCluster::GetHitObject(Int_t idx) const
 {
-   TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
-
-   if (idx >= 0 && idx <  pNtuHit->GetHitsN() )
-      return (TAGobject*)pNtuHit->GetHit(idx);
-
+   if (idx >= 0 && idx <  fpNtuHit->GetHitsN() )
+      return (TAGobject*)fpNtuHit->GetHit(idx);
    else {
-      Error("GetHitObject()", "Error in index %d (max: %d)", idx, pNtuHit->GetHitsN()-1);
+      Error("GetHitObject()", "Error in index %d (max: %d)", idx, fpNtuHit->GetHitsN()-1);
       return 0x0;
    }
 }
@@ -204,7 +289,6 @@ Bool_t TACAactNtuCluster::CreateClusters()
 {
    TACAntuCluster* pNtuClus = (TACAntuCluster*) fpNtuClus->Object();
    TACAparGeo*     pGeoMap  = (TACAparGeo*)     fpGeoMap->Object();
-   TACAntuHit*     pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
 
    TACAcluster* cluster = 0x0;
 
@@ -212,8 +296,8 @@ Bool_t TACAactNtuCluster::CreateClusters()
    for (Int_t i = 0; i< fClustersN; ++i)
       pNtuClus->NewCluster();  //create a TClonesArray of clusters
 
-   for (Int_t i = 0; i < pNtuHit->GetHitsN(); ++i) {
-      TACAhit* hit = pNtuHit->GetHit(i);
+   for (Int_t i = 0; i < fpNtuHit->GetHitsN(); ++i) {
+      TACAhit* hit = fpNtuHit->GetHit(i);
       if (hit == 0x0) continue;
       if (!hit->IsValid()) continue;
       Int_t id = hit->GetCrystalId();
@@ -232,7 +316,7 @@ Bool_t TACAactNtuCluster::CreateClusters()
    // Compute position and fill clusters info
    cluster = 0x0;
 
-   //Fill histos
+   //Fill histograms
    for (Int_t i = 0; i < pNtuClus->GetClustersN(); ++i) {
       cluster = pNtuClus->GetCluster(i);
       FillClusterInfo(cluster);
@@ -255,54 +339,57 @@ Bool_t TACAactNtuCluster::CreateClusters()
 
 }
 
-//______________________________________________________________________________
-//
+///_____________________________________________________________________________
+//! Compute the cluster centroid position
+//!
+//! \param[in] cluster 
 void TACAactNtuCluster::ComputePosition(TACAcluster* cluster)
 {
-   TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
+   //TACAntuHit* pNtuHit  = (TACAntuHit*) fpNtuRaw->Object();
 
    if (cluster->GetListOfHits() == 0) return;
 
-   TVector3 tCorrection, tCorrection2, tCorTemp;
+   TVector3 posWeightSum, posWeightSum2, posWeight;
    TVector3 pos, posErr;
-   tCorrection.SetXYZ( 0., 0., 0.);
-   tCorrection2.SetXYZ( 0., 0., 0.);
+   posWeightSum.SetXYZ( 0., 0., 0.);
+   posWeightSum2.SetXYZ( 0., 0., 0.);
 
-   fClusterPulseSum = 0.;
+   double clusterPulseSum = 0.;
    Float_t maxCharge = 0;
    Int_t iMax = -1;
 
    for (Int_t i = 0; i < cluster->GetHitsN(); ++i) {
       TACAhit* hit = cluster->GetHit(i);
-      tCorTemp.SetXYZ(hit->GetPosition()(0)*hit->GetCharge(), hit->GetPosition()(1)*hit->GetCharge(), hit->GetPosition()(2)*hit->GetCharge());
-      tCorrection  += tCorTemp;
-      fClusterPulseSum  += hit->GetCharge();
+      posWeight.SetXYZ(hit->GetPosition()(0)*hit->GetCharge(), hit->GetPosition()(1)*hit->GetCharge(), hit->GetPosition()(2)*hit->GetCharge());
+      posWeightSum  += posWeight;
+      clusterPulseSum  += hit->GetCharge();
 
       if (hit->GetCharge() > maxCharge) {
          maxCharge = hit->GetCharge();
          iMax      = i;
       }
    }
-   pos = tCorrection*(1./fClusterPulseSum);
+   pos = posWeightSum*(1./clusterPulseSum);
 
    for (Int_t i = 0; i < cluster->GetHitsN(); ++i) {
      TACAhit* hit = cluster->GetHit(i);
-     tCorrection2.SetXYZ(hit->GetCharge()*(hit->GetPosition()(0)-(pos)(0))*(hit->GetPosition()(0)-(pos)(0)),
-			 hit->GetCharge()*(hit->GetPosition()(1)-(pos)(1))*(hit->GetPosition()(1)-(pos)(1)),
-			 0);
-     posErr += tCorrection2;
+     posWeightSum2.SetXYZ(
+         hit->GetCharge()*(hit->GetPosition()(0)-(pos)(0))*(hit->GetPosition()(0)-(pos)(0)),
+         hit->GetCharge()*(hit->GetPosition()(1)-(pos)(1))*(hit->GetPosition()(1)-(pos)(1)),
+         0);
+     posErr += posWeightSum2;
    }
    
-   posErr *= 1./fClusterPulseSum;
+   posErr *= 1./clusterPulseSum;
 
-   fCurrentPosition.SetXYZ(pos[0], pos[1], pos[2]);
-   fCurrentPosError.SetXYZ(TMath::Sqrt(posErr[0]), TMath::Sqrt(posErr[1]), 0);
+   //fCurrentPosition.SetXYZ(pos[0], pos[1], pos[2]);
+   //fCurrentPosError.SetXYZ(TMath::Sqrt(posErr[0]), TMath::Sqrt(posErr[1]), 0);
 
    cluster->SetIndexSeed(iMax);
-   cluster->SetPosition(fCurrentPosition);
-   cluster->SetPosError(fCurrentPosError);
-   cluster->SetPositionG(fCurrentPosition);
-   cluster->SetEnergy(fClusterPulseSum);
+   cluster->SetPosition(pos);
+   cluster->SetPosError(posErr);
+   cluster->SetPositionG(pos);
+   cluster->SetEnergy(clusterPulseSum);
 }
 
 //______________________________________________________________________________
@@ -311,11 +398,11 @@ void TACAactNtuCluster::FillClusterInfo(TACAcluster* cluster)
 {
    ComputePosition(cluster);
 
-   if (ApplyCuts(cluster)) {
+   //if (ApplyCuts(cluster)) {
       if (fpNtuTwPoint)
          ComputeMinDist(cluster);
 
-      // histogramms
+      // histograms
       if (ValidHistogram()) {
          if (cluster->GetHitsN() > 0) {
             fpHisHitTot->Fill(cluster->GetHitsN());
@@ -325,9 +412,9 @@ void TACAactNtuCluster::FillClusterInfo(TACAcluster* cluster)
       }
 
       cluster->SetValid(true);
-   } else {
-      cluster->SetValid(false);
-   }
+   //} else {
+   //   cluster->SetValid(false);
+   //}
 }
 
 //______________________________________________________________________________
@@ -365,7 +452,7 @@ void TACAactNtuCluster::ComputeMinDist(TACAcluster* cluster)
       }
    } //end loop on points
 
-   if(imin != -1) {
+   if (imin != -1) {
       resMin[0]  += gRandom->Uniform(-1, 1);
       resMin[1]  += gRandom->Uniform(-1, 1);
       TATWpoint *point = pNtuPoint->GetPoint(imin);
