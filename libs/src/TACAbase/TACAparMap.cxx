94e23e7e1a25f49cba184be27b0f88db7f60d9d5
lopeztor <ernesto.lopez@to.infn.it>
2022-07-14 16:27:12 +0200

    Parameter for Arduino temp map added

--- libs/src/TACAbase/TACAparMap.cxx
+++ libs/src/TACAbase/TACAparMap.cxx
@@ -40,53 +40,86 @@ TACAparMap::~TACAparMap()
 //! Read mapping data from file \a name .
 Bool_t TACAparMap::FromFile(const TString& name)
 {
-  Clear();
-  
-  if (!Open(name))
-    return false;
-  
-  // read for parameter
-  Double_t* para = new Double_t[5];
-  // Int_t nCrys = 0;
+   Clear();
+   
+   if (!Open(name))
+      return false;
+   
+   // read for parameter
+   Double_t* para = new Double_t[5];
 
-  // number of crystal
-  ReadItem(nCrys);
+   // number of crystal
+   ReadItem(fnCrys);
 
-  if (FootDebugLevel(1)) {
-    printf("CrystalsN: %d\n", nCrys);
-    printf("CrystalId ModuleId ChannelId BoardId ActiveCrystal \n");
-  }
+   if (FootDebugLevel(1)) {
+      printf("CrystalsN: %d\n", fnCrys);
+      printf("CrystalId ModuleId ChannelId BoardId ActiveCrystal \n");
+   }
 
-  //To read header
-  ReadItem(para, 5, ' ', false);  
-  // cout << "n crys: " << nCrys << endl;
-  for (Int_t i = 0; i < nCrys; ++i) { // Loop over crystal
+   // To read header
+   ReadItem(para, 5, ' ', false);  
+   // cout << "n crys: " << nCrys << endl;
+   for (Int_t i = 0; i < fnCrys; ++i) { // Loop over crystal
 
-    // read parameters (boardId chId, crysId)
-    ReadItem(para, 5, ' ', false);
-  
-    // fill map
-    Int_t crysId      = TMath::Nint(para[0]);
-    Int_t moduleId    = TMath::Nint(para[1]);
-    Int_t channelId   = TMath::Nint(para[2]);
-    Int_t boardId     = TMath::Nint(para[3]);
-    Int_t activeCrys  = TMath::Nint(para[4]);
-    
-    pair<int, int> idx(boardId, channelId);
-    fCrysId[idx] = crysId;
-    
-    fModuleId.push_back(moduleId);
-    fBoardId.push_back(boardId);
-    fChannelId.push_back(channelId);
-
-    if (FootDebugLevel(1))
-      printf("%2d %2d %2d B%2d %d\n", crysId, moduleId, channelId, boardId, activeCrys);
+      // read parameters (boardId chId, crysId)
+      ReadItem(para, 5, ' ', false);
+   
+      // fill map
+      Int_t crysId      = TMath::Nint(para[0]);
+      Int_t moduleId    = TMath::Nint(para[1]);
+      Int_t channelId   = TMath::Nint(para[2]);
+      Int_t boardId     = TMath::Nint(para[3]);
+      Int_t activeCrys  = TMath::Nint(para[4]);
       
-  }
+      pair<int, int> idx(boardId, channelId);
+      fCrysId[idx] = crysId;
+      
+      fModuleId.push_back(moduleId);
+      fBoardId.push_back(boardId);
+      fChannelId.push_back(channelId);
 
-  delete [] para;
+      if (FootDebugLevel(1))
+         printf("%2d %2d %2d B%2d %d\n", crysId, moduleId, channelId, boardId, activeCrys);
+         
+   }
 
-  return true;
+
+
+   // Read Arduino/temp parameters
+   if ( !Eof()) {
+
+      ReadItem(para, 5, ' ', false);  // To read header
+      ReadItem(para, 4, ' ', false);  // To read header
+      for (Int_t i = 0; i < fnCrys && !Eof(); ++i) { // Loop over crystal
+         // read parameters (cryId, boardId, mux, chId, crysId)
+         ReadItem(para, 4, ' ', false);
+
+         Int_t crysIdA      = TMath::Nint(para[0]);
+         Int_t boardIdA     = TMath::Nint(para[1]);
+         Int_t muxnum       = TMath::Nint(para[2]);
+         Int_t channelIdA   = TMath::Nint(para[3]);
+
+         if (boardIdA < 1 || boardIdA > 4 
+            || muxnum < 0 || muxnum > 4
+            || channelIdA < 0 || channelIdA > 15) {
+            Error("FromFile", "Arduino parameter out of range. board %d mux %d channel %d", boardIdA, muxnum, channelIdA);
+            continue;
+         }
+
+         // add if it is not found
+         if ( GetArduinoCrystalId(boardIdA, muxnum,channelIdA ) == -1) {
+            pair<int, int> idx(muxnum, channelIdA);
+            fCrysIdArduino[boardIdA-1][idx] = crysIdA;
+         } else {
+            Error("FromFile", "Arduino parameter duplicated. board %d mux %d channel %d", boardIdA, muxnum, channelIdA);
+            continue;
+         }
+      }
+   }
+
+   delete [] para;
+
+   return true;
 }
 
 //------------------------------------------+-----------------------------------
@@ -94,21 +127,37 @@ Bool_t TACAparMap::FromFile(const TString& name)
 
 void TACAparMap::Clear(Option_t*)
 {
-  fCrysId.clear();
+   fCrysId.clear();
+   fCrysIdArduino[0].clear();
+   fCrysIdArduino[1].clear();
+   fCrysIdArduino[2].clear();
+   fCrysIdArduino[3].clear();
 }
 
 //------------------------------------------+-----------------------------------
 Int_t TACAparMap::GetCrystalId(Int_t boardId, Int_t channelId)
 {
-  pair<int, int> idx(boardId, channelId);
-  
-  if (fCrysId.count(make_pair(boardId, channelId))) {
-     auto itr = fCrysId.find(make_pair(boardId, channelId));
-     return fCrysId[idx];
-  }
+   pair<int, int> idx(boardId, channelId);
+   
+   if (fCrysId.count(idx)) {
+      //auto itr = fCrysId.find(make_pair(boardId, channelId));
+      return fCrysId[idx];
+   }
+   
+   return -1;
+}
 
-  //if (itr == fCrysId.end())
-    return -1;
-  
-  //return fCrysId[idx];
+//------------------------------------------+-----------------------------------
+Int_t TACAparMap::GetArduinoCrystalId(Int_t boardID, Int_t muxnum, Int_t ch)
+{
+   if (boardID < 1 || boardID > 4 
+       || muxnum < 0 || muxnum > 4
+       || ch < 0 || ch > 15) return -1;
+
+   pair<int, int> idx(muxnum, ch);
+   if (fCrysIdArduino[boardID-1].count(idx)) {
+      return fCrysIdArduino[boardID-1][idx];
+   }
+
+   return -1;
 }
--- libs/src/TACAbase/TACAparMap.hxx
+++ libs/src/TACAbase/TACAparMap.hxx
@@ -20,30 +20,33 @@ using namespace std;
 class TACAparMap : public TAGparTools {
 public:
 
-  TACAparMap();
-    virtual         ~TACAparMap();
+                    TACAparMap();
+   virtual         ~TACAparMap();
 
-    // void            FromFile(std::string FileName);
-    Bool_t          FromFile(const TString& name);
+   // void            FromFile(std::string FileName);
+   Bool_t          FromFile(const TString& name);
 
-    void            Clear(Option_t* opt="");
+   void            Clear(Option_t* opt="");
 
-    Int_t           GetCrystalId(Int_t boardId, Int_t channelId);
-    // Int_t           GetCrystalsN() {return nCrystals;}
-    Int_t           GetCrystalsN() const {return nCrys;}
-    Int_t           GetBoardId(Int_t cryId) {return fBoardId[cryId]; }
-    Int_t           GetChannelId(Int_t cryId) {return fChannelId[cryId]; }
-    Int_t           GetModuleId(Int_t cryId) {return fModuleId[cryId]; }
+   Int_t           GetArduinoCrystalId(Int_t boardID, Int_t muxnum, Int_t ch);
+   Int_t           GetCrystalId(Int_t boardId, Int_t channelId);
+   // Int_t           GetCrystalsN() {return nCrystals;}
+   Int_t           GetCrystalsN() const {return fnCrys;}
+   Int_t           GetBoardId(Int_t cryId) {return fBoardId[cryId]; }
+   Int_t           GetChannelId(Int_t cryId) {return fChannelId[cryId]; }
+   Int_t           GetModuleId(Int_t cryId) {return fModuleId[cryId]; }
 
 private:
-  // Int_t nCrystals;
-  Int_t nCrys;
-  map< pair<int, int>, int > fCrysId;
-  vector<Int_t> fModuleId;
-  vector<Int_t> fBoardId;
-  vector<Int_t> fChannelId;
-  
-  ClassDef(TACAparMap,1)
+   Int_t                       fnCrys;
+   map< pair<int, int>, int >  fCrysIdArduino[4]; // map crystal ID Arduino setup (maximum 4 boards)
+   map< pair<int, int>, int >  fCrysId;           // map crystal ID to WD boards
+
+   vector<Int_t>               fModuleId;
+   vector<Int_t>               fBoardId;
+   vector<Int_t>               fChannelId;
+
+   
+   ClassDef(TACAparMap, 2)
 
 };
 
