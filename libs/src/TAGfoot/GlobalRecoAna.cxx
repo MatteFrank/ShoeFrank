93f7d365c3c958b3d97a3b2aae684372405d69bf
Christian Finck <cfinck@iphc.cnrs.fr>
2022-09-01 09:54:10 +0200

    Correct a typo

--- libs/src/TAGfoot/GlobalRecoAna.cxx
+++ libs/src/TAGfoot/GlobalRecoAna.cxx
@@ -275,7 +275,7 @@ void GlobalRecoAna::LoopEvent() {
                   }
                 }
                 
-            } else   if  (indexSize = 1) {                
+            } else   if  (indexSize == 1) {                
                 myfile <<"----------------------------"<<endl<<"TW GHOST CHECK - McTracks = 1"<<endl;
                 myfile << "row id "<<rowHit->GetMcTrackIdx(0)<<" POS: "<<rowHit->GetPosition() << " and column id "<<colHit->GetMcTrackIdx(0)<<" POS : "<<colHit->GetPosition() << " matching"<<endl;
 
