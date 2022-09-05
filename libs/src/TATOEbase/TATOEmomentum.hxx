06b4a08df3d8067773c1434e0e0abee71608d360
Christian Finck <cfinck@iphc.cnrs.fr>
2022-09-05 09:47:49 +0200

    Cosmetics

--- libs/src/TATOEbase/TATOEmomentum.hxx
+++ libs/src/TATOEbase/TATOEmomentum.hxx
@@ -1,10 +1,3 @@
-
-/*!
- \file TATOEmomentum.hxx
- \brief Declaration of TATOEmomentum
- \author A. Sécher
- */
-
 //
 //File      : TATOEmomentum.hxx
 //Author    : Alexandre Sécher (alexandre.secher@iphc.cnrs.fr)
@@ -12,9 +5,14 @@
 //Framework : PhD thesis, CNRS/IPHC/DRS/DeSis, Strasbourg, France
 //
 
-
 #ifndef _TATOEmomentum_HXX
 #define _TATOEmomentum_HXX
+
+/*!
+ \file TATOEmomentum.hxx
+ \brief Declaration of TATOEmomentum
+ \author A. Sécher
+ */
 
 
 #include "TAGntuGlbTrack.hxx"
@@ -23,16 +21,4 @@
 TVector3 GetMomentumAtZ( TAGtrack* track_ph, double z );
 
 
-
 #endif
-
-
-
-
-
-
-
-
-
-
-
