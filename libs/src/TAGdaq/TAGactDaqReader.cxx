282274949558372a454c8a2e731fe60a8997edf5
Christian Finck <cfinck@iphc.cnrs.fr>
2022-11-07 14:45:41 +0100

    Add ITR

--- libs/src/TAGdaq/TAGactDaqReader.cxx
+++ libs/src/TAGdaq/TAGactDaqReader.cxx
@@ -195,6 +195,11 @@ Bool_t TAGactDaqReader::Process()
    if (evMSD7)
       datDaq->AddFragment(evMSD7);
 
+   // ITR
+   const DECardEvent* evITR = static_cast<const DECardEvent*>(fDaqFileReader->getFragmentID(dataITR | 0x30));
+   if (evITR)
+      datDaq->AddFragment(evITR);
+   
    // WD for ST, TW and CA
    const WDEvent* evWD = static_cast<const WDEvent*>(fDaqFileReader->getFragmentID(dataWD | 0x30));
    if (evWD)
