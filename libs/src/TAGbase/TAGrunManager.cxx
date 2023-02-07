aad83afd56e738b64f3847426bdc0f0f17c9fdb1
Christian Finck <cfinck@iphc.cnrs.fr>
2023-02-07 15:11:31 +0100

    Formatting output

--- libs/src/TAGbase/TAGrunManager.cxx
+++ libs/src/TAGbase/TAGrunManager.cxx
@@ -425,10 +425,10 @@ void TAGrunManager::Print(Option_t* opt) const
    } else if (option.Contains("count")) {
       Int_t sum = 0;
       for (  map<int, int>::const_iterator it = fEvtCounter.begin(); it != fEvtCounter.end(); ++it) {
-         cout << "  Type index:    " << it->first << "  Total Events:  " << it->second << '\n';
+         cout << "  Type index:    " << setw(2) << it->first << "  Total Events:  " << setw(9) << it->second << '\n';
          sum += it->second;
       }
-      cout << "                     Total Events:  " << sum << endl;
+      cout << "                     Total Events:  " << setw(9) << sum << endl;
    }  else {
       RunParameter_t runPar = GetRunPar(fRunNumber);
       Int_t duration = runPar.Duration;
