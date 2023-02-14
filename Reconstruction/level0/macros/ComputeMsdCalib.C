2b5d11050377b03bb1d40091a4ead03588e54ac8
gisilves <gianluigi.silvestre@pg.infn.it>
2023-02-14 17:11:00 +0100

    Fixed misprint in MSD pedestal macro + fixed HIT2022 MSD cfg files

--- Reconstruction/level0/macros/ComputeMsdCalib.C
+++ Reconstruction/level0/macros/ComputeMsdCalib.C
@@ -228,7 +228,7 @@ void ComputeMsdCalib(TString filename = "dataRaw/data_test.00003890.physics_foot
       for (int ch = 0; ch < NChannels; ch++)
       {
          // Fitting histos with gaus to compute ped and raw_sigma
-         if (hADC[sen][ch]->GetEntriesFast())
+         if (hADC[sen][ch]->GetEntries())
          {
             hADC[sen][ch]->Fit("gaus", "QS");
             fittedgaus = (TF1 *)hADC[sen][ch]->GetListOfFunctions()->FindObject("gaus");
@@ -299,7 +299,7 @@ void ComputeMsdCalib(TString filename = "dataRaw/data_test.00003890.physics_foot
       for (int ch = 0; ch < NChannels; ch++)
       {
          // Fitting histos with gaus to compute ped and raw_sigma
-         if (hSignal[sen][ch]->GetEntriesFast())
+         if (hSignal[sen][ch]->GetEntries())
          {
             hSignal[sen][ch]->Fit("gaus", "QS");
             fittedgaus = (TF1 *)hSignal[sen][ch]->GetListOfFunctions()->FindObject("gaus");
