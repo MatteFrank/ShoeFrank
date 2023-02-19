c8277cd176780ed522963592c22de276f4c3a426
Christian Finck <cfinck@iphc.cnrs.fr>
2023-02-19 18:25:28 +0900

    Update crontab

--- Reconstruction/level0/macros/cron.tab.cmake
+++ Reconstruction/level0/macros/cron.tab.cmake
@@ -1,3 +1,1 @@
-SHELL /usr/bin/tcsh
-*/1 * * * * cd @CMAKE_BINARY_DIR@/Reconstruction/level0 && ./nightbuild >> nightbuild.log 2>&1
-
+* * * * * cd @CMAKE_BINARY_DIR@/Reconstruction/level0 && ./nightbuild.csh >> nightbuild.log 2>&1
