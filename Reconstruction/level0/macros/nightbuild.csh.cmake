5122d6ac5273d8265164eef2975bb093755dc9ac
Christian Finck <cfinck@iphc.cnrs.fr>
2023-02-19 20:29:49 +0900

    Add comment echo

--- Reconstruction/level0/macros/nightbuild.csh.cmake
+++ Reconstruction/level0/macros/nightbuild.csh.cmake
@@ -15,24 +15,35 @@ endif
 
 # pull last version
 cd $FOOTSRC
+echo "update newgeom branch"
 git pull origin newgeom_v1.0
+echo " "
 
 #build last verison
+echo "execute cmake & make"
 cd $FOOTBUILD
 cmake $FOOTSRC -DCMAKE_BUILD_TYPE=Debug -DANC_DIR=ON -DFILECOPY=ON
 make -j4
+echo " "
 
-#Run rawdata reconstruction
+#run rawdata reconstruction
 cd $FOOTLEVEL0
+echo "Run reconstruction of raw data"
 DecodeGlb -in $FOOTRAWDATA/data_test.00004306.physics_foot.daq.RAW._lb0000._FOOT-RCD._0001.data -out runGSI2021_4306_Plots_1kEvts.root -nev 1000 -exp GSI2021 -run 4306
+echo " "
 
 #Compare to reference plots
+echo "Compare raw data histograms with reference"
 root -q TestBenchMark.C+\(1\)
+echo " "
 
 #Run MC reconstruction
+echo "Run reconstruction of MC data"
 DecodeGlb -in $FOOTMCDATA/GSI2021/16O_C_400_1_shoereg.root -out runGSI2021_MC_400_Plots_1kEvts.root -nev 1000 -exp GSI2021_MC -run 400 -mc
+echo " "
 
 #Compare to reference plots
+echo "Compare MC data histograms with reference"
 root -q TestBenchMark.C+\(0\)
 
 \mv nightbuild.log nightbuild.old
