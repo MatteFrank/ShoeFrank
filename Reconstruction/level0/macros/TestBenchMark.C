71f828234a61160a2fb9237e23774497d595347f
Christian Finck <cfinck@iphc.cnrs.fr>
2023-02-22 11:41:19 +0100

    Cleaning

--- Reconstruction/level0/macros/TestBenchMark.C
+++ Reconstruction/level0/macros/TestBenchMark.C
@@ -1,4 +1,4 @@
-// Macro to plot tracked cluster size with and w/o target for FIRST data
+// Macro to compare plots with Chi2 test
 // Ch. Finck
 
 
@@ -6,15 +6,11 @@
 
 #include <Riostream.h>
 #include <TH2F.h>
-#include <TF1.h>
+#include <TH1F.h>
 #include <TFile.h>
 #include <TMath.h>
-#include <TCanvas.h>
 #include <TString.h>
 #include <TROOT.h>
-#include <TStyle.h>
-#include <TPaveText.h>
-#include <TLegend.h>
 
 #endif
 
@@ -29,16 +25,15 @@ void TestBenchMark(Bool_t rawData = true)
    
    if (rawData) {
       nameRef = "./data/runGSI2021_4306_RefPlots_1kEvts.root";
-      name = "runGSI2021_4306_Plots_1kEvts.root";
+      name    = "runGSI2021_4306_Plots_1kEvts.root";
    } else {
       nameRef = "./data/runGSI2021_MC_400_RefPlots_1kEvts.root";
-      name = "runGSI2021_MC_400_Plots_1kEvts.root";
+      name    = "runGSI2021_MC_400_Plots_1kEvts.root";
    }
    
    TFile *fRefPlots = new TFile(nameRef.Data());
-   TFile *fPlots = new TFile(name.Data());
+   TFile *fPlots    = new TFile(name.Data());
 
-   
    for (auto const& it : mapName) {
       
       auto vec = it.second;
@@ -46,15 +41,12 @@ void TestBenchMark(Bool_t rawData = true)
          
          TString nameHist = Form("%s/%s", it.first.Data(), itv.Data());
          TH1F* hPixTotRef = (TH1F*)fRefPlots->Get(nameHist.Data());
-         TH1F* hPixTot = (TH1F*)fPlots->Get(nameHist.Data());
+         TH1F* hPixTot    = (TH1F*)fPlots->Get(nameHist.Data());
          
          printf("PValue for detector %s:\n", it.first.Data());
-         
-         Float_t PValue =  hPixTotRef->Chi2Test(hPixTot,"UU P");
+         Float_t PValue   = hPixTotRef->Chi2Test(hPixTot,"UU P");
          
          if (PValue < sigma) printf("Cluster distribution changes for detector %s\n", it.first.Data());
       }
    }
 }
-
-
