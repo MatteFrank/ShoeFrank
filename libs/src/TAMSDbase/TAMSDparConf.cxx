906a7474393dc6e153e74219eae9b97e332bb61f
Christian Finck <cfinck@iphc.cnrs.fr>
2023-02-27 11:11:01 +0100

    Adding tracking algorithm

--- libs/src/TAMSDbase/TAMSDparConf.cxx
+++ libs/src/TAMSDbase/TAMSDparConf.cxx
@@ -59,6 +59,9 @@ Bool_t TAMSDparConf::FromFile(const TString& name)
    if(FootDebugLevel(1))
       cout << " Tracking flag: "<< fAnalysisParameter.TrackingFlag << endl;
    
+   ReadItem(fAnalysisParameter.TrackingAlgo);
+   if(FootDebugLevel(1))
+      cout << "Tracking algorithm: "<< fAnalysisParameter.TrackingAlgo << endl;
    
    ReadItem(fAnalysisParameter.PlanesForTrackMinimum);
    if(FootDebugLevel(1))
--- libs/src/TAMSDbase/TAMSDparConf.hxx
+++ libs/src/TAMSDbase/TAMSDparConf.hxx
@@ -34,6 +34,7 @@ private:
    struct AnalysisParameter_t : public  TObject {
       Bool_t     PedestalFlag;           ///< Pedestal run flag
       Bool_t     TrackingFlag;           ///< Tracking run flag
+      TString    TrackingAlgo;           ///< Tracking algorithm
       Int_t      PlanesForTrackMinimum;  ///< min # planes to build a track in an event
       Float_t    SearchHitDistance;      ///< max distance hit-track to add hit to track
    };
