0d1858e7e168cf6c3718e2373cc1c6ead29c2887
Christian Finck <cfinck@iphc.cnrs.fr>
2023-03-17 17:33:11 +0100

    Send a mail when pattern error

--- Reconstruction/scripts/nightbuild_li.csh.cmake
+++ Reconstruction/scripts/nightbuild_li.csh.cmake
@@ -1,8 +1,15 @@
 #! /bin/tcsh
 #define sbgli server env
 
+set file = 'nightbuild.new'
+set pattern = 'PatternError'
+ 
 setenv FOOTREC /iphc-work/desis/FOOT/build/Reconstruction/
 cd $FOOTREC
 
-echo "sending mail"
-cat nightbuild.new | mail -s "shoe night build" cfinck@iphc.cnrs.fr
+if ( { grep -q  $pattern $file } ) then
+  echo "sending mail"
+  cat nightbuild.new | mail -s "shoe night build" cfinck@iphc.cnrs.fr
+else
+  echo "No error found"
+endif
