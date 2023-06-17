0aab0d5a25ee47c0cd2534e40e120d102febe4a8
Christian Finck <cfinck@iphc.cnrs.fr>
2023-06-17 18:13:41 +0200

    Add default run number

--- Reconstruction/macros/DisplayWave.C
+++ Reconstruction/macros/DisplayWave.C
@@ -9,7 +9,7 @@
 #include "TACEwaveDisplay.hxx"
 #endif
 
-void DisplayWave(TString file = "./data/Run_1200plas_1500plasnew_backTipex_2plas_coinc_24.6MeV_Data_12_10_2021_Binary.bin", TString exp ="TIIM2023", Int_t runNumber)
+void DisplayWave(TString file = "./data/Run_1200plas_1500plasnew_backTipex_2plas_coinc_24.6MeV_Data_12_10_2021_Binary.bin", TString exp ="TIIM2023", Int_t runNumber = 1)
 {
    TACEwaveDisplay::Instance(file, exp, runNumber);
 }
